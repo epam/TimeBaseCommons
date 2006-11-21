377f8ff3f18aaf8f89fb73f98dfb0749d90afd22
jacob <jacob@deltixlab.com>
2006-11-21 23:10:54 +0000

    * Generified Primitive Collections with support for Java Generics * Cleaned up Preprocessing Code to be more readable * Cleaned up ant buildfile to be more readable * Added build.cmd to DELTIX_HOME\bin for command-line build support

---
+++ src/deltix/util/collections/Arrays.java
@@ -0,0 +1,51 @@
+package deltix.util.collections;
+
+/**
+ * @author Jacob Alber
+ */
+public class Arrays {
+    public static <Generic> Generic[] asArray(Generic... array) {
+        return array;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <Generic> Generic[] initializeArray(int size) {
+        return (Generic[]) new Object[size];
+    }
+
+    public static <Generic> void safeArrayCopy(Generic[] src, int srcPos, Generic[] dest, int destPos, int length) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
+    }
+
+    public static void safeArrayCopy(int[] src, int srcPos, int[] dest, int destPos, int length) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
+    }
+
+    public static void safeArrayCopy(byte[] src, int srcPos, byte[] dest, int destPos, int length) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
+    }
+
+    public static void safeArrayCopy(short[] src, int srcPos, short[] dest, int destPos, int length) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
+    }
+
+    public static void safeArrayCopy(long[] src, int srcPos, long[] dest, int destPos, int length) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
+    }
+
+    public static void safeArrayCopy(double[] src, int srcPos, double[] dest, int destPos, int length) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
+    }
+
+    public static void safeArrayCopy(float[] src, int srcPos, float[] dest, int destPos, int length) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
+    }
+
+    public static void safeArrayCopy(char[] src, int srcPos, char[] dest, int destPos, int length) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
+    }
+
+    public static void safeArrayCopy(boolean[] src, int srcPos, boolean[] dest, int destPos, int length) {
+        System.arraycopy(src, srcPos, dest, destPos, length);
+    }
+}
