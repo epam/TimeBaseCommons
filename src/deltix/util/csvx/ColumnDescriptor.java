0a09aadcde1eb7d8f2f5954bf15274fa047df0e4
dbazylev <dbazylev@deltixlab.com>
2008-03-06 13:09:24 +0000

    MemCSVProvider2: irregular data, Gap/Null values, STARTDATE/ENDDATE attributes; domain: irregular cursors fixes

--- src/deltix/util/csvx/ColumnDescriptor.java
+++ src/deltix/util/csvx/ColumnDescriptor.java
@@ -1,7 +1,6 @@
 package deltix.util.csvx;
 
 import deltix.util.Util;
-import deltix.util.collections.CharSubSequence;
 import java.util.regex.*;
 
 /**
@@ -12,25 +11,30 @@ public abstract class ColumnDescriptor {
     private boolean                     mTrim = true;
     private int                         mIdxInCSV = -1;
     private CharSequence                mCell;
-    
-    protected ColumnDescriptor () { 
+
+    private Pattern                     mGapPattern = null;
+    private String []                   mGapValues = null;
+    private Pattern                     mNullPattern = null;
+    private String []                   mNullValues = null;
+
+    protected ColumnDescriptor () {
     }
-    
+
     protected ColumnDescriptor (String header) {
         setHeader (header);
     }
-    
-    public final void           setHeader (String header) {        
+
+    public final void           setHeader (String header) {
         if (header == null)
             throw new IllegalArgumentException ("header == null");
 
         mHeader = header;
     }
-    
+
     public final String         getHeader () {
         return (mHeader);
     }
-    
+
     public final int            getCSVIdx () {
         return (mIdxInCSV);
     }
@@ -38,29 +42,47 @@ public abstract class ColumnDescriptor {
     public final void           setTrimWhiteSpace (boolean flag) {
         mTrim = flag;
     }
-    
+
     public final boolean        getTrimWhiteSpace () {
         return (mTrim);
     }
-    
+
+    public void setGapPattern(String gapPattern) {
+        if (gapPattern != null)
+            mGapPattern = Pattern.compile(gapPattern);
+    }
+
+    public void setGapValues(String[] gapValues) {
+        this.mGapValues = gapValues;
+    }
+
+    public void setNullPattern(String nullPattern) {
+        if (nullPattern != null)
+            mNullPattern = Pattern.compile(nullPattern);
+    }
+
+    public void setNullValues(String[] nullValues) {
+        this.mNullValues = nullValues;
+    }
+
     public final void           fetchCell (CSVXReader csvxrd) {
         assert mIdxInCSV >= 0 : mHeader + ": index unset - call CSVXReader.setIndexFromHeaders (this)!";
-        
+
         mCell = csvxrd.getCell (mIdxInCSV, mTrim);
     }
 
     public final boolean        isEmpty () {
         return (mCell.length () == 0);
     }
-    
+
     public final boolean        equals (CharSequence cs) {
         return (Util.equals (mCell, cs));
     }
-    
+
     public final boolean        matches (Pattern pat) {
         return (pat.matcher (mCell).matches ());
     }
-    
+
     public final CharSequence   getCharSequence () {
         return (mCell);
     }
@@ -68,18 +90,38 @@ public abstract class ColumnDescriptor {
     public final String         getString () {
         return (mCell.toString ());
     }
-      
+
     public final String         getInternedString () {
         return (mCell.toString ().intern ());
     }
-    
+
+    public final boolean isGAP() {
+        boolean result = mCell != null && mGapPattern != null && matches(mGapPattern);
+        if (!result && mCell != null && mGapValues != null) {
+            for (String s : mGapValues)
+                if (s.equals(mCell.toString()))
+                    return true;
+        }
+        return result;
+    }
+
+    public final boolean isNull() {
+        boolean result = mCell != null && mNullPattern != null && matches(mNullPattern);
+        if (!result && mCell != null && mNullValues != null) {
+            for (String s : mNullValues)
+                if (s.equals(mCell.toString()))
+                    return true;
+        }
+        return result;
+    }
+
     public final void           setIndexInCSV (int csvIdx) {
         mIdxInCSV = csvIdx;
     }
 
     public final boolean        findIndexFromHeaders (String [] headers) {
         mIdxInCSV = Util.indexOf (headers, mHeader);
-        
+
         return (mIdxInCSV >= 0);
-    }    
+    }
 }
