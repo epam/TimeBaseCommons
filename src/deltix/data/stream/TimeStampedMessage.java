675166b222293a31071b91efa32c2700631960ce
amalakov <amalakov@deltixlab.com>
2008-07-28 05:09:48 +0000

    Added UHF Scheduler

--- src/deltix/data/stream/TimeStampedMessage.java
+++ src/deltix/data/stream/TimeStampedMessage.java
@@ -5,6 +5,7 @@ package deltix.data.stream;
  */
 public interface TimeStampedMessage {
     public static final long    TIMESTAMP_UNKNOWN = Long.MIN_VALUE;
-    
+
+    /** @return Message timestamp. Time is measured in milliseconds that passed since January 1, 1970 UTC. */
     public long             getTimeStampMs ();
 }
