2d37eb1b84831aabf23627ec67ff1547b920249c
gene <gene@deltixlab.com>
2008-10-10 17:29:46 +0000

    Added DateFormatDetector

--- src/deltix/util/text/DateFormatDetector.java
+++ src/deltix/util/text/DateFormatDetector.java
@@ -1,5 +1,7 @@
 package deltix.util.text;
 
+import deltix.util.lang.StringUtils;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.regex.*;
 
@@ -18,7 +20,7 @@ public class DateFormatDetector {
     
     private static final Pattern    NSEP_DATE_PATTERN = 
         Pattern.compile ("([^\\d]*)\\d\\d\\d\\d\\d\\d\\d\\d(.*)");
-       
+
     private enum TPS {
         BEFORE_HOURS,
         HOURS,
@@ -43,7 +45,57 @@ public class DateFormatDetector {
             out.append (c);
     }
     
-    public static String        getFormatStringFor (CharSequence text) {
+    public static String        getDateFormatStringFor (CharSequence text) {
+        int             limit = text.length ();
+        StringBuilder   timeFormat = new StringBuilder (limit);        
+        int             suffixStart = getDateFormatStringFor (text, timeFormat);
+        
+        if (suffixStart < 0)
+            return (null);
+        
+        for (int ii = suffixStart; ii < limit; ii++) {
+            char        c = text.charAt (ii);
+            
+            if (Character.isDigit (c))
+                return (null);
+            
+            appendToFormat (c, timeFormat);
+        }
+        
+        return (timeFormat.toString ());
+    }
+    
+    private static int          getDateFormatStringFor (
+        CharSequence                text,
+        StringBuilder               timeFormat
+    )
+    {
+        Matcher         m;
+        String          dateFormat;
+        
+        if ((m = SLASH_DATE_PATTERN_MDY.matcher (text)).matches ()) 
+            dateFormat = "M/d/y";
+        else if ((m = SLASH_DATE_PATTERN_YMD.matcher (text)).matches ()) 
+            dateFormat = "y/M/d";
+        else if ((m = DASH_DATE_PATTERN.matcher (text)).matches ())
+            dateFormat = "y-M-d";
+        else if ((m = NSEP_DATE_PATTERN.matcher (text)).matches ()) 
+            dateFormat = "yyyyMMdd";
+        else
+            return (-1);
+        
+        CharSequence    prefix = m.group (1);
+        int             plen = prefix.length ();
+        
+        for (int ii = 0; ii < plen; ii++)
+            appendToFormat (prefix.charAt (ii), timeFormat);
+                
+        timeFormat.append (dateFormat);
+        
+        return (m.start (2));
+    }
+    
+    public static String        getDateTimeFormatStringFor (CharSequence text) {
         Matcher         m;
         String          dateFormat;
         int             limit = text.length ();
@@ -68,59 +120,149 @@ public class DateFormatDetector {
                 
         timeFormat.append (dateFormat);
         
-        int             timeStartIdx = m.start (2);        
-        TPS             state = TPS.BEFORE_HOURS;
+        if (!getTimeFormatStringFor (text, m.start (2), timeFormat))
+            return (null);
+
+        return (timeFormat.toString ());
+    }
+    
+    public static String        getTimeFormatStringFor (CharSequence text) {
+        StringBuilder   timeFormat = new StringBuilder (text.length ());
         
-        for (int ii = timeStartIdx; ii < limit; ii++) {
-            char        c = text.charAt (ii);
-            boolean     isdigit = Character.isDigit (c);
-            
-            switch (state) {
-                case BEFORE_HOURS:  if (isdigit) state = TPS.HOURS; break;                    
-                case HOURS:         if (!isdigit) state = TPS.BEFORE_MINUTES; break;                    
-                case BEFORE_MINUTES: if (isdigit) state = TPS.MINUTES; break;                    
-                case MINUTES:       if (!isdigit) state = TPS.BEFORE_SECONDS; break;                    
-                case BEFORE_SECONDS: if (isdigit) state = TPS.SECONDS; break;                    
-                case SECONDS: if (!isdigit) state = TPS.BEFORE_MILLIS; break;                    
-                case BEFORE_MILLIS: if (isdigit) state = TPS.MILLIS; break;                    
-                case MILLIS: if (!isdigit) state = TPS.STOP; break;                                                                
-            }
-            
-            if (isdigit)
-                switch (state) {
-                    case HOURS:     timeFormat.append ('H'); break;               
-                    case MINUTES:   timeFormat.append ('m'); break;               
-                    case SECONDS:   timeFormat.append ('s'); break;   
-                    case MILLIS:    timeFormat.append ('S'); break;
-                    case STOP:      appendToFormat (c, timeFormat); break;
-                }
-            else
-                appendToFormat (c, timeFormat);
-        }
+        if (!getTimeFormatStringFor (text, 0, timeFormat))
+            return (null);
         
         return (timeFormat.toString ());
     }
+    
+    private static boolean      getTimeFormatStringFor (
+        CharSequence                text, 
+        int                         timeStartIdx,
+        StringBuilder               timeFormat
+    )
+    {
+        int             limit = text.length ();
+        boolean         success = false;
+        TPS             state = TPS.BEFORE_HOURS;
+        
+        for (int ii = timeStartIdx; ii <= limit; ii++) {
+            char        c;
+            boolean     isdigit;
+            
+            if (ii < limit) {
+                c = text.charAt (ii);            
+                isdigit = Character.isDigit (c);
+            }
+            else {
+                c = 0;
+                isdigit = false;
+            }
+            
+            switch (state) {
+                case BEFORE_HOURS:  
+                    if (isdigit) state = TPS.HOURS; 
+                    break; 
+                
+                case HOURS:         
+                    if (!isdigit) 
+                        state = TPS.BEFORE_MINUTES; 
+                    else if (StringUtils.endsWith (timeFormat, "HH"))
+                        state = TPS.MINUTES;
+                    break;                  
+                    
+                case BEFORE_MINUTES: 
+                    if (isdigit) state = TPS.MINUTES; 
+                    break;           
+                    
+                case MINUTES:       
+                    if (!isdigit) 
+                        state = TPS.BEFORE_SECONDS;
+                    else if (StringUtils.endsWith (timeFormat, "mm"))
+                        state = TPS.SECONDS;
+                    break;
+                    
+                case BEFORE_SECONDS:
+                    if (isdigit) state = TPS.SECONDS; 
+                    break;                    
+                    
+                case SECONDS: 
+                    if (!isdigit) 
+                        state = TPS.BEFORE_MILLIS; 
+                    else if (StringUtils.endsWith (timeFormat, "ss"))
+                        state = TPS.MILLIS;
+                    break;                    
+                    
+                case BEFORE_MILLIS: 
+                    if (isdigit) state = TPS.MILLIS; 
+                    break;                    
+                    
+                case MILLIS: 
+                    if (!isdigit)
+                        if (StringUtils.endsWith (timeFormat, "SSS")) 
+                            state = TPS.STOP;
+                        else
+                            return (false); // Prohibit partial millis to avoid false matches with year!
+                    else if (StringUtils.endsWith (timeFormat, "SSS")) 
+                        state = TPS.STOP; 
+                    break;                                                                
+            }
+            
+            if (c == 0)
+                break;
+            
+            if (isdigit)
+                switch (state) {
+                    case HOURS:     
+                        timeFormat.append ('H'); 
+                        success = true;
+                        break;         
+                        
+                    case MINUTES:   timeFormat.append ('m'); break;               
+                    case SECONDS:   timeFormat.append ('s'); break;   
+                    case MILLIS:    timeFormat.append ('S'); break;
+                    case STOP:      
+                        return (false);
+                }
+            else
+                appendToFormat (c, timeFormat);
+        }       
+        
+        return (success);
+    }
+    
+    private static void         testFormat (String s, String header, String fmt) {
+        if (fmt != null) {
+            SimpleDateFormat    df = new SimpleDateFormat (fmt);
+            String              result;
+            
+            try {
+                result = df.format (df.parse (s));
+            } catch (ParseException x) {
+                result = x.getMessage ();
+            }
+            
+            System.out.println ("    " + header + ": " + fmt + " ==> " + result);
+        }
+    }           
     
     public static void          main (String [] args) throws Exception {
         if (args.length == 0)
             args = 
                 new String [] { 
                     "4/2/2008",
-                    "x2008-04-02 23:44",
+                    "15:34",
+                    "x'2008-04-02 23:44",
                     "20080402 1200", 
                     "2008040212:00"
                 };
         
-        for (String s : args) {
-            String              fmt = getFormatStringFor (s);
+        for (String s : args) {            
+            System.out.println (s);
             
-            System.out.println (s + " => " + fmt);
-            
-            if (fmt != null) {
-                SimpleDateFormat    df = new SimpleDateFormat (fmt);
-
-                System.out.println ("    = " + df.parse (s));
-            }
+            testFormat (s, "TIME", getTimeFormatStringFor (s));
+            testFormat (s, "DATE", getDateFormatStringFor (s));
+            testFormat (s, "D/T", getDateTimeFormatStringFor (s));
         }
     }
 }
+
