1c049a9e4c9c543fbd2e13af3e6b0b4a464ffd82
mlandau <mlandau@deltixlab.com>
2008-12-05 09:11:33 +0000

    IB trading service - fix futures order placing

--- src/deltix/util/id/SimpleIdentifierGenerator.java
+++ src/deltix/util/id/SimpleIdentifierGenerator.java
@@ -9,8 +9,13 @@ public class SimpleIdentifierGenerator implements IdentifierGenerator {
 	private long nextID = 1;
 
 	private final int base;
-	public SimpleIdentifierGenerator () {
-	    base = (int) (System.currentTimeMillis() % TimeUnit.DAYS.toMillis(1));
+
+    public SimpleIdentifierGenerator () {
+        this((int) (System.currentTimeMillis() % TimeUnit.DAYS.toMillis(1)));
+    }
+
+    public SimpleIdentifierGenerator(int base) {
+	    this.base = base;
 	}
 
 	@Override
