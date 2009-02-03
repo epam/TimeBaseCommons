8e98c07ba505d4a6a678450182901dcff6ee7736
amalakov <amalakov@deltixlab.com>
2009-02-03 22:00:02 +0000

    Merged revision 5831 from QS 1.2 branch ( svn merge -r 5704:5831 https://gw.deltixlab.com/svn/src/branches/qs-1.2 )

--- src/deltix/qsrv/hf/framework/Disconnectable.java
+++ src/deltix/qsrv/hf/framework/Disconnectable.java
@@ -1,5 +1,11 @@
 package deltix.qsrv.hf.framework;
 
+
+/** 
+ * One of use cases: FeedProvider may optionally implement this interface to notify listener about connection problems 
+ * 
+ * @see DisconnectEventListener
+ */  
 public interface Disconnectable {
 
     void setDisconnectEventListener(DisconnectEventListener listener);
