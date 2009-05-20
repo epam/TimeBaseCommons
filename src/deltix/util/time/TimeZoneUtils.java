1a5195af42b6d4de170f0b7b921cf0520f79ab15
gene <gene@deltixlab.com>
2009-05-20 16:52:58 +0000

    Fixed http://fw.orientsoft.by/bugzilla2/show_bug.cgi?id=5086

--- src/deltix/util/time/TimeZoneUtils.java
+++ src/deltix/util/time/TimeZoneUtils.java
@@ -6,7 +6,28 @@ import java.util.TimeZone;
  *
  */
 public abstract class TimeZoneUtils {
+    private static final long           D = 86400000;
+
     public static long                  getMidnightOn (TimeZone tz, final long absTime) {
-        return (absTime - tz.getOffset (absTime) - absTime % 86400000);
+        /**
+         *  Midnights G in GMT must satisfy     G = D * n
+         *      where n is an integer, D = 86400000
+         *
+         *  Local midnights M must satisfy      M = G - o = D * n - o
+         *      where n is an integer, D = 86400000 and o is the TZ offset
+         *
+         *  Find Mmax = max (M) such that Mmax < absTime
+         *
+         *  n = (absTime + o) / D
+         * 
+         *  Mmax =
+         *      D * n - o =
+         *      (absTime + o) / D * D - o =
+         *      (absTime + o) - (absTime + o) % D - o =
+         *      absTime - (absTime + o) % D
+         */
+        long        o = tz.getOffset (absTime);
+        
+        return (absTime - (absTime + o) % D);
     }
 }
