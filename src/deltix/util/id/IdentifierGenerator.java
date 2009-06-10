211878b93c22055996eb3772e3b730c297945cf2
amalakov <amalakov@deltixlab.com>
2009-06-10 23:24:08 +0000

    suppressing warning for order cancellation confirmation

--- src/deltix/util/id/IdentifierGenerator.java
+++ src/deltix/util/id/IdentifierGenerator.java
@@ -2,5 +2,6 @@ package deltix.util.id;
 
 /** Thread-safe generator of unique integer numbers */
 public interface IdentifierGenerator {
-	long next();
+	/** @returns next valid order id. This method is thread safe */
+    long next();
 }
