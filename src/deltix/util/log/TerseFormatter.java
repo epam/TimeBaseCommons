aec36a414c3cffa2f083ed9899ee36e5d05e5aef
ndul <ndul@deltixlab.com>
2009-08-06 18:00:30 +0000

    merge logging optimization from 4.2

--- src/deltix/util/log/TerseFormatter.java
+++ src/deltix/util/log/TerseFormatter.java
@@ -1,14 +1,11 @@
 package deltix.util.log;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.text.MessageFormat;
-import java.util.logging.Formatter;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-
+import deltix.util.lang.Util;
+import deltix.util.text.SimpleMessageFormat;
 import deltix.util.time.TimeFormatter;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
 
 /**
  * Simple formatter that displays only a log message
@@ -17,82 +14,42 @@ import deltix.util.time.TimeFormatter;
  *
  */
 public class TerseFormatter extends Formatter {
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
     /**
      * Format the given log record and return the formatted string.
      */
     public String format (LogRecord record) {
-
-        // (!) THIS CLASS IS INSTALLED AS JAVA EXTENSION - PLEASE AVOID ADDING ANY DEPENDENCIES.
-
-        StringBuilder sbuf = new StringBuilder();
+        StringBuilder sbuf = new StringBuilder(256);
 
         // time
         long time = record.getMillis();
         if (time != 0) {
-            sbuf.append (formatTimestamp(time));
-            sbuf.append (' ');
+            sbuf.append (formatTimestamp(time)).append(' ');
         }
 
         // level
-        if (record.getLevel() == Level.SEVERE)
-            sbuf.append("(!) ");
+        sbuf.append(record.getLevel()).append(' ');
 
         // message
         String message = record.getMessage ();
         Object [] params = record.getParameters();
         if (params != null) {
-            sbuf.append (MessageFormat.format(message, params));
+            SimpleMessageFormat.format(sbuf, message, params);
         } else {
-            sbuf.append (message);
+            sbuf.append(message);
         }
 
         if (record.getThrown() != null) {
-            sbuf.append ('\n');
-            sbuf.append (getStackTrace (record.getThrown ()));
+            sbuf.append(LINE_SEPARATOR);
+            sbuf.append(Util.printStackTrace(record.getThrown()));
         }
 
-        sbuf.append('\n');
+        sbuf.append(LINE_SEPARATOR);
         return sbuf.toString();
     }
 
     protected String formatTimestamp(long time) {
         return TimeFormatter.formatTimeOfDayGMT(time);
     }
-
-    /** Prints stack trace of given throwable, unwraps any ChainedException */
-    private static String getStackTrace (Throwable t) {
-
-        StringWriter sw = new StringWriter (1024);
-        try {
-            PrintWriter pwr = new PrintWriter (sw);
-
-            printStackTrace (t, pwr);
-
-            pwr.close ();
-            sw.close ();
-
-        } catch (IOException ignore) { ignore.printStackTrace();   }
-
-        return sw.toString();
-    }
-
-    /** Prints stack trace of given throwable, unwraps any ChainedException */
-    private static void printStackTrace (Throwable t, PrintWriter pwr) {
-
-        if (t != null)
-            t.printStackTrace(pwr);
-
-        Throwable cause = null;
-        if (t != null) {
-            cause = t.getCause();
-        }
-
-        if (cause != null) {
-            pwr.println ("\nCaused by:\n");
-            printStackTrace (cause, pwr);
-        }
-    }
-
-
 }
