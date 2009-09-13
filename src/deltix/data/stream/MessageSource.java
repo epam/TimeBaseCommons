54753e6e0a76c77d10e995236a3afa0c70b45920
amalakov <amalakov@deltixlab.com>
2009-09-13 00:44:59 +0000

    Merged 4.2 changes

--- src/deltix/data/stream/MessageSource.java
+++ src/deltix/data/stream/MessageSource.java
@@ -6,5 +6,9 @@ import deltix.util.concurrent.AbstractCursor;
  *
  */
 public interface MessageSource <T> extends AbstractCursor {
+
+    /*
+    * @return current message located by {@link AbstractCursor#next()} method call. Use {@link AbstractCursor#next()} to scroll cursor to the next message.
+    */
     public T            getMessage ();
 }
