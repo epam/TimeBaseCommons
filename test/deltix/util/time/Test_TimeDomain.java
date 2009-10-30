7927cbfe67804766b7f92e995e67d97a45340ce7
andy <amalakov@deltixlab.com>
2009-10-30 22:14:04 +0000

    made this test quiet

--- test/deltix/util/time/Test_TimeDomain.java
+++ test/deltix/util/time/Test_TimeDomain.java
@@ -7,7 +7,9 @@ import static org.junit.Assert.*;
 
 public class Test_TimeDomain {
     private TimeDomain     td;
-    
+
+    private static boolean quiet = Boolean.getBoolean("quiet");
+
     @Before
     public void     setUp () {
         td = new TimeDomain ();
@@ -23,7 +25,8 @@ public class Test_TimeDomain {
 
         td.add (34, 46);
 
-        System.out.println (td);
+        if ( ! quiet)
+            System.out.println (td);
     }
     
     @Test
@@ -33,7 +36,59 @@ public class Test_TimeDomain {
         assertTrue (td.includes (19));
         assertFalse (td.includes (25));
     }    
-    
+
+
+    private final String [] expected = {
+            "0 -> X",
+            "1 -> X",
+            "2 -> X",
+            "3 -> X",
+            "4 -> X",
+            "5 -> X",
+            "6 -> X",
+            "7 -> X",
+            "8 -> X",
+            "9 -> X",
+            "10 -> 0",
+            "11 -> 1",
+            "12 -> 2",
+            "13 -> 3",
+            "14 -> 4",
+            "15 -> 5",
+            "16 -> 6",
+            "17 -> 7",
+            "18 -> 8",
+            "19 -> 9",
+            "20 -> 10",
+            "21 -> 11",
+            "22 -> 12",
+            "23 -> 13",
+            "24 -> 14",
+            "25 -> X",
+            "26 -> X",
+            "27 -> X",
+            "28 -> X",
+            "29 -> X",
+            "30 -> X",
+            "31 -> X",
+            "32 -> X",
+            "33 -> X",
+            "34 -> 15",
+            "35 -> 16",
+            "36 -> 17",
+            "37 -> 18",
+            "38 -> 19",
+            "39 -> 20",
+            "40 -> 21",
+            "41 -> 22",
+            "42 -> 23",
+            "43 -> 24",
+            "44 -> 25",
+            "45 -> 26",
+            "46 -> X",
+            "47 -> X",
+            "48 -> X",
+            "49 -> X" };
     @Test
     public void     testXForm () {
         td.buildIndex ();
@@ -41,16 +96,21 @@ public class Test_TimeDomain {
         for (int ii = 0; ii < 50; ii++) {
             long    o = td.globalToOpen (ii, false);
 
+            String actual;
             if (o == TimeDomain.TIME_EXCLUDED) {
-                System.out.println (ii + " -> X");
+                actual = (ii + " -> X");
                 continue;
+            } else {
+                long    t = td.openToGlobal (o);
+                assertEquals (ii, t);
+                actual = (ii + " -> " + o);
             }
 
-            System.out.println (ii + " -> " + o);
+            if ( ! quiet)
+                System.out.println (actual);
 
-            long    t = td.openToGlobal (o);
+            assertEquals (expected[ii], actual);
 
-            assertEquals (ii, t);
         }
     }
 }
