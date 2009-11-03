575a4a21b969e24ece5b34e6582411f786b09dd3
ndul <ndul@deltixlab.com>
2009-11-03 12:22:22 +0000

    aggregator - fix bug 6065

--- src/deltix/qsrv/hf/framework/DisconnectableEventHandler.java
+++ src/deltix/qsrv/hf/framework/DisconnectableEventHandler.java
@@ -1,28 +1,20 @@
 package deltix.qsrv.hf.framework;
 
-import net.jcip.annotations.GuardedBy;
-
-import java.util.List;
-import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Helps implement the {@link Disconnectable} interface.
- * <p> Doesn't maintain a connection status, so <code>isConnected</code> must be implemented by a client.
- * </p> 
+ * <p> Doesn't maintain a connection status, so <code>isConnected</code> must be implemented by a client.</p>
  */
 public class DisconnectableEventHandler implements Disconnectable {
-    @GuardedBy("this")
-    private final List<DisconnectEventListener> listeners = new ArrayList<DisconnectEventListener>();
+    private final CopyOnWriteArrayList<DisconnectEventListener> listeners =
+        new CopyOnWriteArrayList<DisconnectEventListener>();
 
-    public synchronized void addDisconnectEventListener(
-        DisconnectEventListener listener
-    ) {
-        listeners.add(listener);
+    public void addDisconnectEventListener(DisconnectEventListener listener) {
+        listeners.addIfAbsent(listener);
     }
 
-    public synchronized void removeDisconnectEventListener(
-        DisconnectEventListener listener
-    ) {
+    public void removeDisconnectEventListener(DisconnectEventListener listener) {
         listeners.remove(listener);
     }
 
@@ -30,17 +22,15 @@ public class DisconnectableEventHandler implements Disconnectable {
         throw new UnsupportedOperationException();
     }
 
-    public synchronized void onReconnected() {
-        if (!listeners.isEmpty())
-            for (int i = 0; i < listeners.size(); i++) {
-                listeners.get(i).onReconnected();
-            }
+    public void onReconnected() {
+        for (DisconnectEventListener listener : listeners) {
+            listener.onReconnected();
+        }
     }
 
-    public synchronized void onDisconnected() {
-        if (!listeners.isEmpty())
-            for (int i = 0; i < listeners.size(); i++) {
-                listeners.get(i).onDisconnected();
-            }
+    public void onDisconnected() {
+        for (DisconnectEventListener listener : listeners) {
+            listener.onDisconnected();
+        }
     }
 }
