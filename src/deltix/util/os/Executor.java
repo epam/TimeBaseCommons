50b48cc1e810131968030b476b315fbdd918ec46
akarpovich <akarpovich@deltixlab.com>
2010-05-28 16:06:52 +0000

    [-] 6137, 7379 fix

---
+++ src/deltix/util/os/Executor.java
@@ -0,0 +1,75 @@
+package deltix.util.os;
+
+import deltix.util.lang.Util;
+
+import java.io.*;
+
+/**
+ * Date: May 28, 2010
+ *
+ * @author alex
+ */
+public class Executor {
+
+    static class StreamGrabber extends Thread {
+        private InputStream is;
+        private PrintStream out;
+
+        StreamGrabber(InputStream is, PrintStream out) {
+            this.is = is;
+            this.out = out;
+        }
+
+        public void run() {
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(new InputStreamReader(is));
+                String line;
+                while ( (line = reader.readLine()) != null)
+                    out.println(line);
+
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            } finally {
+                Util.close(reader);
+            }
+        }
+    }    
+
+    /**
+     * Executes the specified file.
+     *
+     * @param   file      a specified file to execute     
+     *
+     * @throws  SecurityException
+     *          If a security manager exists and its
+     *          {@link SecurityManager#checkExec checkExec}
+     *          method doesn't allow creation of the subprocess
+     *
+     * @throws  IOException
+     *          If an I/O error occurs
+     *
+     * @throws  NullPointerException
+     *          If <code>command</code> is <code>null</code>
+     *
+     * @throws  IllegalArgumentException
+     *          If <code>command</code> is empty
+     *
+     * @see     Runtime#exec(String[], String[], java.io.File)
+     * @see     ProcessBuilder
+     */
+
+    public static void exec(File file) throws IOException {
+        (new ProcessBuilder("cmd.exe", "/C", "start", file.getPath())).start();         
+    }
+
+    public static void exec(ProcessBuilder builder, boolean redirect) throws IOException {
+        Process process = builder.start();
+        
+        if (redirect) {
+            new StreamGrabber(process.getErrorStream(), System.err).start();
+            new StreamGrabber(process.getInputStream(), System.out).start();
+        }
+    }
+
+}
