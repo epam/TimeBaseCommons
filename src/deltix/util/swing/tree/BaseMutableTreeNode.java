0f9fcb3906fd46863e17636a03c15979735f9309
kpaharelau <kpaharelau@deltixlab.com>
2010-08-30 08:44:38 +0000

    fix bugs 7987, 7988

--- src/deltix/util/swing/tree/BaseMutableTreeNode.java
+++ src/deltix/util/swing/tree/BaseMutableTreeNode.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.util.*;
 
 import javax.swing.*;
+import javax.swing.event.*;
 import javax.swing.tree.*;
 
 import com.jidesoft.plaf.basic.*;
@@ -12,37 +13,37 @@ import com.jidesoft.tree.*;
 
 public abstract class BaseMutableTreeNode extends LazyMutableTreeNode {
 
-    public static Locale LOCALE = Locale.getDefault ( );
+    public static Locale LOCALE = Locale.getDefault ();
 
-    public static final Object findTreeNode ( final JTree tree,
-                                              final Object userObject ) {
-        final Object root = tree.getModel ( ).getRoot ( );
+    public static final Object findTreeNode (final JTree tree,
+                                              final Object userObject) {
+        final Object root = tree.getModel ().getRoot ();
         // Traverse tree from root
-        return findTreeNode ( tree,
-                              new TreePath ( root ),
-                              userObject );
+        return findTreeNode (tree,
+                              new TreePath (root),
+                              userObject);
     }
 
-    public static final Object findTreeNode ( final JTree tree,
+    public static final Object findTreeNode (final JTree tree,
                                               final TreePath parent,
-                                              final Object userObject ) {
+                                              final Object userObject) {
         // Traverse children
-        final Object node = parent.getLastPathComponent ( );
+        final Object node = parent.getLastPathComponent ();
 
         if (node instanceof BaseMutableTreeNode
-            && JideSwingUtilities.equals ( userObject,
-                                           ((BaseMutableTreeNode) node).getUserObject ( ) )) {
+            && JideSwingUtilities.equals (userObject,
+                                           ((BaseMutableTreeNode) node).getUserObject ())) {
             return node;
         }
 
-        if (tree.getModel ( ).getChildCount ( node ) >= 0) {
-            for (int i = 0; i < tree.getModel ( ).getChildCount ( node ); i++) {
-                final Object n = tree.getModel ( ).getChild ( node,
-                                                              i );
-                final TreePath path = parent.pathByAddingChild ( n );
-                final Object result = findTreeNode ( tree,
+        if (tree.getModel ().getChildCount (node) >= 0) {
+            for (int i = 0; i < tree.getModel ().getChildCount (node); i++) {
+                final Object n = tree.getModel ().getChild (node,
+                                                              i);
+                final TreePath path = parent.pathByAddingChild (n);
+                final Object result = findTreeNode (tree,
                                                      path,
-                                                     userObject );
+                                                     userObject);
                 if (result != null) {
                     return result;
                 }
@@ -51,64 +52,64 @@ public abstract class BaseMutableTreeNode extends LazyMutableTreeNode {
         return null;
     }
 
-    protected final JTree      _tree;
+    protected final JTree _tree;
 
-    public BaseMutableTreeNode ( final Object userObject,
+    public BaseMutableTreeNode (final Object userObject,
                                  final boolean allowsChildren,
-                                 final JTree tree ) {
-        super ( userObject,
-                allowsChildren );
+                                 final JTree tree) {
+        super (userObject,
+                allowsChildren);
         _tree = tree;
-        createMenu ( );
+        createMenu ();
     }
 
-    public BaseMutableTreeNode ( final Object userObject,
-                                 final JTree tree ) {
-        super ( userObject );
+    public BaseMutableTreeNode (final Object userObject,
+                                 final JTree tree) {
+        super (userObject);
         _tree = tree;
-        createMenu ( );
+        createMenu ();
     }
 
-    protected void createMenu ( ) {
+    protected void createMenu () {
     }
 
-    public final JTree getTree ( ) {
+    public final JTree getTree () {
         return _tree;
     }
 
-    protected final void updateChildren ( ) {
+    protected final void updateChildren () {
         synchronized (this) {
             if (!_loaded) {
                 _loaded = true;
-                initChildren ( );
+                initChildren ();
             }
         }
     }
 
-    public void reload ( ) {
-        clear ( );
-        updateChildren ( );
-        refreshTree ( );
+    public void reload () {
+        clear ();
+        updateChildren ();
+        refreshTree ();
     }
 
-    public String getLabelText ( ) {
-        return userObject.toString ( );
+    public String getLabelText () {
+        return userObject.toString ();
     }
 
-    public Icon getIcon ( ) {
+    public Icon getIcon () {
         return null;
     }
 
-    protected String getTooltip ( ) {
+    protected String getTooltip () {
         return null;
     }
 
-    public TreePath getTreePath ( ) {
+    public TreePath getTreePath () {
         int depth = 1;
         TreeNode node = this;
 
         for (;;) {
-            final TreeNode next = node.getParent ( );
+            final TreeNode next = node.getParent ();
 
             if (next == null)
                 break;
@@ -128,126 +129,168 @@ public abstract class BaseMutableTreeNode extends LazyMutableTreeNode {
             if (depth == 0)
                 break;
 
-            node = node.getParent ( );
+            node = node.getParent ();
         }
 
-        return (new TreePath ( path ));
+        return (new TreePath (path));
     }
 
-    protected TreeModel getActualModel ( ) {
-        TreeModel model = _tree.getModel ( );
+    protected TreeModel getActualModel () {
+        TreeModel model = _tree.getModel ();
 
-        final FilterableTreeModel filterableTreeModel = getFilterableTreeModel ( );
+        final FilterableTreeModel filterableTreeModel = getFilterableTreeModel ();
         if (filterableTreeModel != null) {
-            model = filterableTreeModel.getActualModel ( );
+            model = filterableTreeModel.getActualModel ();
         }
 
         return model;
     }
 
-    protected FilterableTreeModel getFilterableTreeModel ( ) {
-        final TreeModel model = _tree.getModel ( );
+    protected FilterableTreeModel getFilterableTreeModel () {
+        final TreeModel model = _tree.getModel ();
         if (model instanceof FilterableTreeModel) {
             return (FilterableTreeModel) model;
         }
         return null;
     }
 
-    protected DefaultTreeModel getActualDefaultTreeModel ( ) {
-        final TreeModel model = getActualModel ( );
+    protected DefaultTreeModel getActualDefaultTreeModel () {
+        final TreeModel model = getActualModel ();
         if (model instanceof DefaultTreeModel) {
             return (DefaultTreeModel) model;
         }
         return null;
     }
 
-    public void delete ( ) {
-        final DefaultTreeModel model = getActualDefaultTreeModel ( );
+    public void delete () {
+        final DefaultTreeModel model = getActualDefaultTreeModel ();
         if (model != null) {
-            model.removeNodeFromParent ( this );
+            model.removeNodeFromParent (this);
         }
-        refreshTree ( );
+        refreshTree ();
     }
 
-    public final void refreshTree ( ) {
-        final FilterableTreeModel _displayTreeModel = getFilterableTreeModel ();
-        if (_displayTreeModel != null) {
+    public final void refreshTree () {
 
-            // save selection and expansion states
-            Enumeration<TreePath> enumeration = null;
-            TreePath[] selected = null;
-            if (getTree ( ) != null) {
-                enumeration = TreeUtils.saveExpansionStateByTreePath ( getTree ( ) );
-                selected = TreeUtils.saveSelection ( getTree ( ) );
+        // save selection and expansion states
+        Enumeration<TreePath> enumeration = null;
+        TreePath[] selected = null;
+        if (getTree () != null) {
+            enumeration = TreeUtils.saveExpansionStateByTreePath (getTree ());
+            selected = TreeUtils.saveSelection (getTree ());
+        }
+
+        refresh ();
+
+        // restore selection and expansion states
+        if (getTree () != null) {
+            if (enumeration != null) {
+                TreeUtils.loadExpansionStateByTreePath (getTree (),
+                                                             enumeration);
             }
-
-            _displayTreeModel.refresh ( );
-
-            // restore selection and expansion states
-            if (getTree ( ) != null) {
-                if (enumeration != null) {
-                    TreeUtils.loadExpansionStateByTreePath ( getTree ( ),
-                                                             enumeration );
-                }
-                if (selected != null) {
-                    TreeUtils.loadSelection ( getTree ( ),
-                                              selected );
-                }
+            if (selected != null) {
+                TreeUtils.loadSelection (getTree (), selected);
             }
         }
+
     }
 
-    public void nodeChanged ( ) {
-        final DefaultTreeModel model = getActualDefaultTreeModel ( );
-        if (model != null) {
-            model.nodeChanged ( this );
-        }
-    }
-
-    public void nodeStructureChanged ( ) {
-        final DefaultTreeModel model = getActualDefaultTreeModel ( );
-        if (model != null) {
-            model.nodeStructureChanged ( this );
-        }
-    }
-
-    public final void insertNodeInto ( final BaseMutableTreeNode node ) {
-        final DefaultTreeModel model = getActualDefaultTreeModel ( );
-        if (model != null) {
-            model.insertNodeInto ( node,
-                                   this,
-                                   this.getChildCount ( ) );
+    private void refresh () {
+        final FilterableTreeModel displayTreeModel = getFilterableTreeModel ();
+        if (displayTreeModel != null) {
+            displayTreeModel.refresh ();
         } else {
-            reload ( );
+            final TreeModel model = _tree.getModel ();
+            if (model instanceof AbstractTreeModel)
+                fireTreeStructureChanged ((AbstractTreeModel) model, new TreePath (getRoot ()));
+            else if (model instanceof DefaultTreeModel)
+                ((DefaultTreeModel) model).reload ();
         }
-
-        add ( node );
     }
 
-    public final void select ( ) {
+    protected void fireTreeStructureChanged (AbstractTreeModel source, TreePath path) {
+        // Guaranteed to return a non-null array
+        Object[] listeners = source.getTreeModelListeners ();
+        TreeModelEvent e = null;
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TreeModelListener.class) {
+                // Lazily create the event:
+                if (e == null)
+                    e = new TreeModelEvent (source,
+                                            path);
+                ((TreeModelListener) listeners[i + 1]).treeStructureChanged (e);
+            }
+        }
+    }
+
+    protected void fireTreeStructureChanged (DefaultTreeModel source, TreePath path) {
+        // Guaranteed to return a non-null array
+        Object[] listeners = source.getTreeModelListeners ();
+        TreeModelEvent e = null;
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TreeModelListener.class) {
+                // Lazily create the event:
+                if (e == null)
+                    e = new TreeModelEvent (source,
+                                            path);
+                ((TreeModelListener) listeners[i + 1]).treeStructureChanged (e);
+            }
+        }
+    }
+
+    public void nodeChanged () {
+        final DefaultTreeModel model = getActualDefaultTreeModel ();
+        if (model != null) {
+            model.nodeChanged (this);
+        }
+    }
+
+    public void nodeStructureChanged () {
+        final DefaultTreeModel model = getActualDefaultTreeModel ();
+        if (model != null) {
+            model.nodeStructureChanged (this);
+        }
+    }
+
+    public final void insertNodeInto (final BaseMutableTreeNode node) {
+        final DefaultTreeModel model = getActualDefaultTreeModel ();
+        if (model != null) {
+            model.insertNodeInto (node,
+                                   this,
+                                   this.getChildCount ());
+        } else {
+            reload ();
+        }
+
+        add (node);
+    }
+
+    public final void select () {
         /**
          * Reset selection momentarily in order to force the reloading of the
          * node's form.
          */
-        _tree.setSelectionPath ( null );
-        final TreePath path = getTreePath ( );
-        _tree.setSelectionPath ( path );
-        _tree.expandPath ( path );
+        _tree.setSelectionPath (null);
+        final TreePath path = getTreePath ();
+        _tree.setSelectionPath (path);
+        _tree.expandPath (path);
     }
-
- 
 
     /**
      * Return the font for the label to display.
      */
-    protected Font getFont ( final Font defFont ) {
+    protected Font getFont (final Font defFont) {
         return (defFont);
     }
 
     /**
      * Return the color for the label to display, or null.
      */
-    protected Color getColor ( ) {
+    protected Color getColor () {
         return (null);
     }
 
@@ -255,77 +298,77 @@ public abstract class BaseMutableTreeNode extends LazyMutableTreeNode {
      * User can override this method instead of {@link #configureLabel}, to
      * realize the full power of JTree.
      */
-    private JComponent render ( final boolean selected,
+    private JComponent render (final boolean selected,
                                 final boolean hasFocus,
-                                final JLabel defaultRendering ) {
-        configureLabel ( defaultRendering,
+                                final JLabel defaultRendering) {
+        configureLabel (defaultRendering,
                          selected,
-                         hasFocus );
+                         hasFocus);
         return (defaultRendering);
     }
 
     /**
-     * User can override this method instead of {@link #getColor},
-     * {@link #getFont}, {@link #getLabelText} and {@link #getIcon}. Default
+     * User can override this method instead of {@link #getColor}, {@link #getFont},
+     * {@link #getLabelText} and {@link #getIcon}. Default
      * implementation sets the above properties in the label.
      */
-    public void configureLabel ( final JLabel label,
+    public void configureLabel (final JLabel label,
                                  final boolean selected,
-                                 final boolean hasFocus ) {
+                                 final boolean hasFocus) {
 
-        final Color c = getColor ( );
-        final Font f = getFont ( label.getFont ( ) );
+        final Color c = getColor ();
+        final Font f = getFont (label.getFont ());
 
         if (c != null)
-            label.setForeground ( c );
+            label.setForeground (c);
 
         if (f != null)
-            label.setFont ( f );
+            label.setFont (f);
 
-        final Icon icon = getIcon ( );
+        final Icon icon = getIcon ();
 
-        label.setIcon ( icon );
-        label.setDisabledIcon ( icon );
-        label.setText ( getLabelText ( ) );
+        label.setIcon (icon);
+        label.setDisabledIcon (icon);
+        label.setText (getLabelText ());
     }
 
     public static class NodeRenderer extends DefaultTreeCellRenderer {
 
-        private final Font _defaultFont = getFont ( );
+        private final Font _defaultFont = getFont ();
 
         @Override
-        public Component getTreeCellRendererComponent ( final JTree jTree,
+        public Component getTreeCellRendererComponent (final JTree jTree,
                                                         final Object node,
                                                         final boolean selected,
                                                         final boolean expanded,
                                                         final boolean leaf,
                                                         final int row,
-                                                        final boolean hasFocus ) {
+                                                        final boolean hasFocus) {
 
-            final JLabel label = (JLabel) super.getTreeCellRendererComponent ( jTree,
+            final JLabel label = (JLabel) super.getTreeCellRendererComponent (jTree,
                                                                                node,
                                                                                selected,
                                                                                expanded,
                                                                                leaf,
                                                                                row,
-                                                                               hasFocus );
+                                                                               hasFocus);
 
             if (node instanceof BaseMutableTreeNode) {
 
-                final String tootip = ((BaseMutableTreeNode) node).getTooltip ( );
+                final String tootip = ((BaseMutableTreeNode) node).getTooltip ();
                 if (tootip != null)
-                    setToolTipText ( tootip );
+                    setToolTipText (tootip);
             }
 
             /**
              * Reset font because it may have been tweaked
              */
-            label.setFont ( _defaultFont );
+            label.setFont (_defaultFont);
 
             final BaseMutableTreeNode anode = (BaseMutableTreeNode) node;
-            return anode.render ( selected,
+            return anode.render (selected,
                                   hasFocus,
-                                  label );
+                                  label);
         }
     }
 }
