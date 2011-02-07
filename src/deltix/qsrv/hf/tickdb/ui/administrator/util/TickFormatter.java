61e38b069745c3b532e1156c14862df82ab7191d
kpaharelau <kpaharelau@deltixlab.com>
2011-02-07 09:08:18 +0000

    ETL refactoring

--- src/deltix/qsrv/hf/tickdb/ui/administrator/util/TickFormatter.java
+++ src/deltix/qsrv/hf/tickdb/ui/administrator/util/TickFormatter.java
@@ -7,45 +7,44 @@ import java.util.logging.Formatter;
 
 public class TickFormatter extends Formatter{
     
-    Date dat = new Date();
-    private final static String format = "{0,date} {0,time}";
-    private java.text.MessageFormat formatter;
-    
-    // Line separator string.  This is the value of the line.separator
-    // property at the moment that the SimpleFormatter was created.
-    private String lineSeparator = "\r\n";
-    
-    private Object args[] = new Object[1];
+    private              Date                    date          = new Date ();
+    private static final String                  format        = "{0,date} {0,time}";
+    private              java.text.MessageFormat formatter;
+    private              String                  lineSeparator = "\r\n";
+    private              Object                  args[]        = new Object[1];
+    private              StringBuffer            buffer        = new StringBuffer ();
+    private              StringBuffer            text          = new StringBuffer ();
 
+    @SuppressWarnings({"ThrowableResultOfMethodCallIgnored"})
     @Override
     public String format(LogRecord record) {
-        StringBuffer sb = new StringBuffer();
+        buffer.setLength (0);
+        text.setLength (0);
         // Minimize memory allocations here.
-        dat.setTime(record.getMillis());
-        args[0] = dat;
-        StringBuffer text = new StringBuffer();
+        date.setTime(record.getMillis());
+        args[0] = date;
         if (formatter == null) {
             formatter = new MessageFormat(format);
         }
         formatter.format(args, text, null);
-        sb.append(text);
-        sb.append(" ");
+        buffer.append (text);
+        buffer.append (" ");
         
         String message = formatMessage(record);
-        sb.append(record.getLevel().getLocalizedName());
-        sb.append(": ");
+        buffer.append (record.getLevel ().getLocalizedName ());
+        buffer.append (": ");
         if (message != null)
-            sb.append(message);
+            buffer.append (message);
 
-        sb.append(lineSeparator);
-         
-        if (record.getThrown() != null) {
-            String          msg = record.getThrown().getLocalizedMessage ();
-            sb.append("Reason: ").append(msg != null ? msg : record.getThrown());
-            sb.append(lineSeparator);
+        buffer.append (lineSeparator);
+
+        final Throwable throwable = record.getThrown ();
+        if (throwable != null) {
+            buffer.append (throwable.toString ());
+            buffer.append (lineSeparator);
         }
         
-        return sb.toString();
+        return buffer.toString ();
     }
 
 }
