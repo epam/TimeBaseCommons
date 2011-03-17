66ccb4285578be3bdce805ba3b620c82a7a50c83
andy <amalakov@deltixlab.com>
2011-03-17 02:27:07 +0000

    Enhanced UHF monitor, StategyCloner tool, misc

--- src/deltix/util/collections/TwoWayMap.java
+++ src/deltix/util/collections/TwoWayMap.java
@@ -1,32 +1,45 @@
 package deltix.util.collections;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public class TwoWayMap {
-	private final HashMap<Object, Object> firstToSecond = new HashMap<Object, Object> ( );
-	private final HashMap<Object, Object> secondToFirst = new HashMap<Object, Object> ( );
+public class TwoWayMap<T1, T2> {
+	private final Map<T1, T2> firstToSecond = new HashMap<T1, T2> ( );
+	private final Map<T2, T1> secondToFirst = new HashMap<T2, T1> ( );
 
-	public void put ( Object first,
-	                  Object second ) {
+    public TwoWayMap () {
+    }
+
+    public TwoWayMap (Map<T1,T2> prototype) {
+        for (Map.Entry<T1,T2> entry : prototype.entrySet()) {
+            T1 k = entry.getKey();
+            T2 v = entry.getValue();
+
+            firstToSecond.put(k, v);
+            secondToFirst.put(v, k);
+        }
+    }
+
+	public void put ( T1 first, T2 second ) {
 		firstToSecond.put ( first,
 		                    second );
 		secondToFirst.put ( second,
 		                    first );
 	}
 
-	public final HashMap<Object, Object> getFirstToSecond ( ) {
+	public final Map<T1, T2> getFirstToSecond ( ) {
 		return firstToSecond;
 	}
 
-	public final HashMap<Object, Object> getSecondToFirst ( ) {
+	public final Map<T2, T1> getSecondToFirst ( ) {
 		return secondToFirst;
 	}
 
-	public Object getFirst ( Object first ) {
+	public T2 getFirst ( T1 first ) {
 		return firstToSecond.get ( first );
 	}
 
-	public Object getSecond ( Object second ) {
+	public T1 getSecond ( T2 second ) {
 		return secondToFirst.get ( second );
 	}
 
