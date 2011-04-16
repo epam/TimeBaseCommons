12d2d9377d10b2573e0d40362bb3606352042bb2
akarpovich <akarpovich@deltixlab.com>
2011-04-16 20:25:39 +0000

    [*] QS Admin fixes

---
+++ src/deltix/util/os/ExecutionException.java
@@ -0,0 +1,19 @@
+package deltix.util.os;
+
+import java.io.IOException;
+
+public class ExecutionException extends IOException {
+    private int errorCode;
+    
+    public ExecutionException() {
+    }
+
+    public ExecutionException(String message, int errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public int          getErrorCode() {
+        return errorCode;
+    }
+}
