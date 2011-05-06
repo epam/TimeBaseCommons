3573139ff36737b0b9ddfb2d56e44ef476090534
gene <gene@deltixlab.com>
2011-05-06 20:59:58 +0000

    minor

--- src/deltix/qsrv/hf/spi/conn/DisconnectableImpl.java
+++ src/deltix/qsrv/hf/spi/conn/DisconnectableImpl.java
@@ -1,17 +1,17 @@
 package deltix.qsrv.hf.spi.conn;
 
-import deltix.qsrv.hf.spi.conn.DisconnectableEventHandler;
-
 /**
  *  Helps implement the {@link deltix.qsrv.hf.spi.conn.Disconnectable} interface.
  */
 public class DisconnectableImpl extends DisconnectableEventHandler {
     private volatile boolean isConnected = false;
 
+    @Override
     public boolean isConnected() {
         return isConnected;
     }
 
+    @Override
     public void onReconnected() {
         if (!isConnected) {
             isConnected = true;
@@ -19,6 +19,7 @@ public class DisconnectableImpl extends DisconnectableEventHandler {
         }
     }
 
+    @Override
     public void onDisconnected() {
         if (isConnected) {
             isConnected = false;
