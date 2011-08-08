5ca9bcdc4d88c6117a260ce58c68713d0887d049
sturskiy <sturskiy@deltixlab.com>
2011-08-08 19:55:38 +0000

    [-] 9900, mapping alphanumeric(10) type as long

--- src/deltix/util/text/CharSequenceParser.java
+++ src/deltix/util/text/CharSequenceParser.java
@@ -1,6 +1,5 @@
 package deltix.util.text;
 
-import deltix.qsrv.hf.pub.ExchangeCodec;
 import deltix.util.lang.Util;
 
 /**
@@ -117,19 +116,10 @@ public abstract class CharSequenceParser {
     }
     
     public static long  parseLong (CharSequence sc) {
-        long value = -1;
-        //it is temporary solution to parse ALPHANUMERIC strings
-        try
-        {
-            value = parseLong (sc, 0, sc.length ());
-        }
-        catch (NumberFormatException e){
-           value = ExchangeCodec.codeToLong(sc);
-        }
-        return value;
+        return (parseLong (sc, 0, sc.length ()));
     }
     
-    public static long  parseLong (CharSequence sc, final int startIncl, final int endExcl) {
+    public static long  parseLong (final CharSequence sc, final int startIncl, final int endExcl) {
         if (startIncl > endExcl)
             throw new IllegalArgumentException ("Illegal range: " + startIncl + ".." + endExcl);
         
@@ -153,8 +143,8 @@ public abstract class CharSequenceParser {
         
         for (;;) {
             if (ch != ',') {
+                int             digit = ch - '0';
 
-                int digit = ch - '0';
                 if (digit < 0 || digit > 9)
                     throw new NumberFormatException (
                         "Illegal digit at position " + (pos + 1) + " in: " + sc.subSequence (startIncl, endExcl).toString ());
