693116923316029c14e45fa8de5287e9b16b6a9a
ndul <ndul@deltixlab.com>
2011-08-22 16:20:08 +0000

    [*] Trading Latency - add moving min/max values

--- test/deltix/qsrv/hf/framework/trade/latency/Test_SimpleStatistics.java
+++ test/deltix/qsrv/hf/framework/trade/latency/Test_SimpleStatistics.java
@@ -22,13 +22,21 @@ public class Test_SimpleStatistics {
         for (int i = 0; i < values.length; i++) {
             stat.add(values[i]);
 
+            double movingMax = Double.MIN_VALUE;
+            double movingMin = Double.MAX_VALUE;
             double sum = 0d;
             int start = Math.max(0, i - movingAvgSize + 1);
             for (int j = start; j <= i; j++) {
                 sum += values[j];
+                movingMin = Math.min(movingMin, values[j]);
+                movingMax = Math.max(movingMax, values[j]);
             }
+
+            double[] minMax = stat.getMovingMinMax();
             double movingAvg = sum / Math.min(i + 1, movingAvgSize);
 
+            Assert.assertEquals(movingMin, minMax[0], 0.0001);
+            Assert.assertEquals(movingMax, minMax[1], 0.0001);
             Assert.assertEquals(movingAvg, stat.getMovingAvg(), 0.0001);
         }
     }
