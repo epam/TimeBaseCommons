e144ba9383c9056d4bf849a71ad30ed8d305c433
akarpovich <akarpovich@deltixlab.com>
2011-12-22 15:30:46 +0000

    [-] 11003

--- src/deltix/util/swing/tree/BaseTreeNode.java
+++ src/deltix/util/swing/tree/BaseTreeNode.java
@@ -167,6 +167,7 @@ public abstract class BaseTreeNode<T> implements TreeNode {
     }
     
     @Override
+    @SuppressWarnings("unchecked")
     public Enumeration<?> children() {
         updateChildren();
         return (new ArrayEnumeration(mChildNodes.toArray()));
