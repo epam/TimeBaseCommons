57cceb7a0ad4c7d3ee19b88e932080ba30e06e9f
dbazylev <dbazylev@deltixlab.com>
2012-01-09 20:14:08 +0000

    #10425, #11000, #11096

--- src/deltix/util/log/SMTPHandler.java
+++ src/deltix/util/log/SMTPHandler.java
@@ -85,7 +85,7 @@ public class SMTPHandler extends Handler {
 
         setTo(getProperty(manager, "to", null));
         setFrom(getProperty(manager, "from", null));
-        setSmtpHost(getProperty(manager, "host", null));
+        setSmtpHost(getProperty(manager, "smtpHost", null));
         setSmtpPort(parseInt(getProperty(manager, "smtpPort", null), 0));
         setSmtpSecure(Boolean.valueOf(getProperty(manager, "smtpSecure", "false")));
         setSmtpUsername(getProperty(manager, "smtpUsername", null));
