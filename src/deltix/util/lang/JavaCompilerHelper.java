4455345f85ff7ba455d55ed63897486936bcb9d0
dbazylev <dbazylev@deltixlab.com>
2012-01-20 16:17:08 +0000

    qsadmin + dcedit integration; #11133, #11128

--- src/deltix/util/lang/JavaCompilerHelper.java
+++ src/deltix/util/lang/JavaCompilerHelper.java
@@ -59,7 +59,7 @@ public class JavaCompilerHelper {
         final String optionString = System.getProperty("javac.options");
         final Iterable<String> options = optionString == null ? null : Arrays.asList(optionString.split(" "));
         JavaCompiler.CompilationTask compile = JAVA_COMPILER_INSTANCE.getTask(out, fileManager, dianosticListener, options, null, compilationUnits);
-        boolean ok;
+        final boolean ok;
         synchronized (JAVA_COMPILER_INSTANCE) {
             ok = compile.call();
         }
@@ -95,7 +95,10 @@ public class JavaCompilerHelper {
         final String optionString = System.getProperty("javac.options");
         final Iterable<String> options = optionString == null ? null : Arrays.asList(optionString.split(" "));
         JavaCompiler.CompilationTask compile = JAVA_COMPILER_INSTANCE.getTask(out, fileManager, dianosticListener, options, null, compilationUnits);
-        boolean ok = compile.call();
+        final boolean ok;
+        synchronized (JAVA_COMPILER_INSTANCE) {
+            ok = compile.call();
+        }
 
         final boolean hasDiagnostic = dianosticListener.getDiagnostics().size() > 0;
         StringBuilder sb = null;
