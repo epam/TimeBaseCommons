90a7ba7b77d57000d85e288debf64f1f6d641fd5
ndul <ndul@deltixlab.com>
2012-08-20 09:58:02 +0000

    [+] Trading Connector Wizard (standalone version) - tctest.cmd

---
+++ src/deltix/util/xml/PropertiesAdapterEx.java
@@ -0,0 +1,59 @@
+package deltix.util.xml;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PropertiesAdapterEx extends XmlAdapter<PropertiesAdapterEx.PropertyList, Properties> {
+    @Override
+    public Properties unmarshal(PropertyList v) throws Exception {
+        Properties result = new Properties();
+        for (int i = 0; i < v.property.size(); i++) {
+            PropertyEntry prop = v.property.get(i);
+            result.setProperty(prop.key, prop.value);
+        }
+        return result;
+    }
+
+    @Override
+    public PropertyList marshal(Properties v) throws Exception {
+        PropertyList propList = new PropertyList();
+        propList.property = new ArrayList<PropertyEntry>(v.size());
+        for (String key : v.stringPropertyNames()) {
+            String value = v.getProperty(key);
+            if (value != null && value.length() > 0)
+                propList.property.add(new PropertyEntry(key, value));
+        }
+        return propList;
+    }
+
+    //////////////////////// HELPER CLASSES ///////////////////////
+
+    @XmlType
+    public static final class PropertyList {
+        @XmlElement
+        List<PropertyEntry> property;
+    }
+
+    @XmlType
+    public static final class PropertyEntry {
+        @XmlAttribute
+        String key;
+
+        @XmlValue
+        String value;
+
+        public PropertyEntry() {
+        }
+
+        public PropertyEntry(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
