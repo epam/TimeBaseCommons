578cf2e22273aed3df92f47397dfa6479068abd9
agudkov <agudkov@deltixlab.com>
2012-11-20 12:17:43 +0000

    Old-style IQFeed has been removed

---
+++ src/deltix/util/lang/NotFoundException.java
@@ -0,0 +1,24 @@
+package deltix.util.lang;
+
+public class NotFoundException extends Exception {
+
+    public NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException() {
+    }   
+    
+}
