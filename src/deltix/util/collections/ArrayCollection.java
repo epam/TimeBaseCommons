ab2d78132c1dfb5199f5f77e7f3882ebc450a89b
ndul <ndul@deltixlab.com>
2013-01-08 13:35:47 +0000

    [*] supress Java7 warnings

--- src/deltix/util/collections/ArrayCollection.java
+++ src/deltix/util/collections/ArrayCollection.java
@@ -8,6 +8,7 @@ import java.util.*;
 public class ArrayCollection <T> extends AbstractCollection <T> {
     private final T []        array;
 
+    @SafeVarargs
     public ArrayCollection (T ... array) {
         this.array = array;
     }
