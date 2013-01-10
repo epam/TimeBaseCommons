75842c4a5f24b5b0b743d122248546770d31dde4
ndul <ndul@deltixlab.com>
2013-01-10 14:33:02 +0000

    [*] supress Java7 warnings

--- src/deltix/util/event/ListenerSupport.java
+++ src/deltix/util/event/ListenerSupport.java
@@ -8,11 +8,12 @@ public class ListenerSupport<L> {
     private final Class<L> componentType;
     private volatile L[] listeners;
 
-    @SuppressWarnings("unchecked")
-    public ListenerSupport(Class<L> componentType, L ... listeners) {
+    @SafeVarargs
+    @SuppressWarnings("varargs")
+    public ListenerSupport(Class<L> componentType, L... listeners) {
         this.componentType = componentType;
         if (listeners != null && listeners.length > 0) {
-            this.listeners = (L[]) newArray(componentType, listeners.length);
+            this.listeners = newArray(componentType, listeners.length);
             System.arraycopy(listeners, 0, this.listeners, 0, listeners.length);
         }
     }
@@ -23,11 +24,10 @@ public class ListenerSupport<L> {
         listeners = arrayadd(componentType, listeners, listener);
     }
 
-    @SuppressWarnings("unchecked")
     public synchronized void removeListener(L listener) {
         if (listener == null)
             return;
-        listeners = (L[]) Util.arraydel(listeners, listener);
+        listeners = Util.arraydel(listeners, listener);
     }
 
     public synchronized void removeAllListeners() {
@@ -52,17 +52,16 @@ public class ListenerSupport<L> {
     }
 
     @SuppressWarnings("unchecked")
-    private static <T> T[] newArray(Class<? super T> componentType, int length) {
+    private static <T> T[] newArray(Class<T> componentType, int length) {
         return (T[]) Array.newInstance(componentType, length);
     }
 
-    @SuppressWarnings("unchecked")
-    private static <T> T[] arrayadd(Class<? super T> compType, T[] arr, T newItem) {
+    private static <T> T[] arrayadd(Class<T> compType, T[] arr, T newItem) {
         T[] newarr;
         if (arr == null || arr.length <= 0) {
-            newarr = (T[]) newArray(compType, 1);
+            newarr = newArray(compType, 1);
         } else {
-            newarr = (T[]) newArray(compType, arr.length + 1);
+            newarr = newArray(compType, arr.length + 1);
             System.arraycopy(arr, 0, newarr, 0, arr.length);
         }
         newarr[newarr.length - 1] = newItem;
