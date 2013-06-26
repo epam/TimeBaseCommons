6d08c7448e0fd9fc72182a28cdcb044ce01fcb65
gene <gene@deltixlab.com>
2013-06-26 16:25:45 +0000

    expdocs, build pubdocs

--- src/deltix/util/id/IdentifierGenerator.java
+++ src/deltix/util/id/IdentifierGenerator.java
@@ -2,7 +2,7 @@ package deltix.util.id;
 
 /** Thread-safe generator of unique integer numbers */
 public interface IdentifierGenerator {
-	/** @returns next valid order id. This method is thread safe */
+	/** @return next valid order id. This method is thread safe */
     long next();
 
     /**
