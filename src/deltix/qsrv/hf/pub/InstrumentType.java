4f54c065e86813bd3157da08cde0b80e92f92b44
gene <gene@deltixlab.com>
2013-07-23 17:31:07 +0000

    Cleaned up jcg library, it is now possible to generate C# code from JRE.

--- src/deltix/qsrv/hf/pub/InstrumentType.java
+++ src/deltix/qsrv/hf/pub/InstrumentType.java
@@ -58,7 +58,7 @@ public enum InstrumentType {
     /** @return true if instrument of this type can accumulate positions and appear in trade orders */
     public boolean isTradable() {
         int ordinal = ordinal();
-        return (ordinal <= SIMPLE_OPTION.ordinal() || ordinal == SPREAD.ordinal());
+        return (ordinal <= SIMPLE_OPTION.ordinal() || this == SPREAD);
     }
     
 }
