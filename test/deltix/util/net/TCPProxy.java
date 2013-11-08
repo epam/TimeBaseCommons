d69efce7332fe76138c63fa005e99fef08b4c19e
andy <amalakov@deltixlab.com>
2013-11-08 13:44:58 +0000

    Latency test

--- test/deltix/util/net/TCPProxy.java
+++ test/deltix/util/net/TCPProxy.java
@@ -38,26 +38,46 @@ public class TCPProxy {
     }
 
     public static void main (String [] args) throws Exception {
-        if (args.length != 3) {
-            System.out.println ("Command line args: <bind port> <proxy host> <proxy port>");
+        if (args.length == 0) {
+            System.out.println ("Command line args:\n\t<bind port> <proxy host> <proxy port>\nOR\n\t<src host> <src port> <proxy host> <proxy port>");
             return;
         }
 
-        int                 bindPort = Integer.parseInt (args [0]);
-        String              proxyHost = args[1];
-        int                 proxyPort = Integer.parseInt (args [2]);
+        if (args.length == 3) {
+            int bindPort = Integer.parseInt (args [0]);
+            String proxyHost = args[1];
+            int proxyPort = Integer.parseInt (args [2]);
+            runServer(bindPort, proxyHost, proxyPort);
+        } else {
+            String srcHost = args [0];
+            int srcPort = Integer.parseInt (args [1]);
+            String proxyHost = args[2];
+            int proxyPort = Integer.parseInt (args [3]);
+            runClient(srcHost, srcPort, proxyHost, proxyPort);
+        }
+    }
 
+    private static void runClient(String srcHost, int srcPort, String proxyHost, int proxyPort) throws IOException {
+        Socket input = new Socket (srcHost, srcPort);
+        input.setKeepAlive(true);
+        input.setTcpNoDelay(true);
+        Socket output = new Socket (proxyHost, proxyPort);
+        output.setKeepAlive(true);
+        output.setTcpNoDelay(true);
+
+        System.out.println ("Connected and ready");
+
+        new Proxy(input.getInputStream (), output.getOutputStream ()).start();
+    }
+
+    private static void runServer(int bindPort, String proxyHost, int proxyPort) throws IOException {
         ServerSocket ss = new ServerSocket (bindPort);
-
         System.out.println ("Proxy is listening on port " + ss.getLocalPort ());
-
-
         while (true) {
-            Socket s = ss.accept ();
-
-            Socket          delegate = new Socket (proxyHost, proxyPort);
-             System.out.println ("Accepted " + delegate);
-            new Proxy(s.getInputStream (), delegate.getOutputStream ()).start();
+            Socket input = ss.accept ();
+            Socket output = new Socket (proxyHost, proxyPort);
+            System.out.println ("Accepted " + output);
+            new Proxy(input.getInputStream (), output.getOutputStream ()).start();
         }
     }
 
