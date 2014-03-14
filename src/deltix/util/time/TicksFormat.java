7ffb2fb532342e5e166a9aa8800067346003c3cb
akarpovich <akarpovich@deltixlab.com>
2014-03-14 11:46:55 +0000

    [*] nanotime format corrected

--- src/deltix/util/time/TicksFormat.java
+++ src/deltix/util/time/TicksFormat.java
@@ -2,31 +2,33 @@ package deltix.util.time;
 
 import deltix.data.stream.TimeStampedMessage;
 import deltix.qsrv.hf.pub.TimeStamp;
+import org.apache.commons.lang.time.FastDateFormat;
 
 import java.text.DecimalFormat;
 import java.text.FieldPosition;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 public class TicksFormat {
 
-    private static final SimpleDateFormat   TICKS = new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss.");
-    private static final SimpleDateFormat   MS = new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss.S");
+    private final StringBuffer              sb = new StringBuffer();
+    private final Calendar                  calendar = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
+
+    private static final FastDateFormat     TICKS = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss.", TimeZone.getTimeZone("GMT"));
+    private static final FastDateFormat     MS = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss.SSS", TimeZone.getTimeZone("GMT"));
     private static final DecimalFormat      DF = new DecimalFormat("000000000");
     
-    static {
-        MS.setTimeZone(TimeZone.getTimeZone("GMT"));
-        TICKS.setTimeZone(TimeZone.getTimeZone("GMT"));
-    }
-    
-    public String         format(long milliseconds, int nanosComponent) {
-        StringBuffer sb = new StringBuffer();
+    public synchronized String         format(long milliseconds, int nanosComponent) {
+        sb.setLength(0);
+        calendar.setTimeInMillis(milliseconds);
+
         if (nanosComponent == 0 || milliseconds == TimeStampedMessage.TIMESTAMP_UNKNOWN) {
-            MS.format(new Date(milliseconds), sb, new FieldPosition(0));
+            MS.format(calendar, sb);
         } else {
+            TICKS.format(calendar, sb);
+
             long ms = milliseconds % 1000;
-            TICKS.format(new Date(milliseconds), sb, new FieldPosition(0));
             DF.format(ms * TimeStamp.NANOS_PER_MS + nanosComponent, sb, new FieldPosition(0));
         }
 
