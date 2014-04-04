0dce51a05745739df98a9bc9639e562c2ab7640c
akarpovich <akarpovich@deltixlab.com>
2014-04-04 12:13:07 +0000

    [-] 12106 fix

--- src/deltix/qsrv/hf/tickdb/comm/client/Signal.java
+++ src/deltix/qsrv/hf/tickdb/comm/client/Signal.java
@@ -5,19 +5,35 @@ package deltix.qsrv.hf.tickdb.comm.client;
  */
 public class Signal {
     private boolean             state = false;
+    private Throwable           error;
 
     public synchronized void    set() {
         state = true;
+        error = null;
+
         notify();
+    }
+
+    public synchronized void    set(Throwable e) {
+        state = true;
+        error = e;
+
+        notify();
+    }
+
+    public synchronized void    verify() throws IllegalStateException {
+        if (error != null)
+            throw new IllegalStateException(error);
     }
 
     public synchronized void    reset() {
         state = false;
+        error = null;
     }
 
     public synchronized boolean await() throws InterruptedException {
         while (!state)
-            wait(10000);
+            wait(5000);
 
         state = false;
         return true;
