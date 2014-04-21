fc7e9bb06ba0ae6e709a4b94fb3069151d8d6289
akorzun <akorzun@deltixlab.com>
2014-04-21 15:35:15 +0000

    polishing

--- src/deltix/util/log/gf/ForwardingHandler.java
+++ src/deltix/util/log/gf/ForwardingHandler.java
@@ -37,17 +37,16 @@ public class ForwardingHandler extends Handler {
     public void publish(LogRecord record) {
         Level level = getLevel(record.getLevel());
 
-        if (!LOGGER.isLoggable(level))
-            throw new IllegalStateException("GF Logging is not properly configured. All levels must be loggable.");
+        if (LOGGER.isLoggable(level)) {
+            String msg = getMsg(record);
+            LogEntry entry = LOGGER.level(level).append(msg);
 
-        String msg = getMsg(record);
-        LogEntry entry = LOGGER.level(level).append(msg);
+            Throwable exception = record.getThrown();
+            if (exception != null)
+                entry.append(exception);
 
-        Throwable throwable = record.getThrown();
-        if (throwable != null)
-            entry.append(throwable);
-
-        entry.commit();
+            entry.commit();
+        }
     }
 
     @Override
