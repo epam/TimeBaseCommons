43cb675eec0f316a85b96c73d5fd2727668bc89f
akorzun <akorzun@deltixlab.com>
2014-04-23 16:38:17 +0000

    added supporting compound appenders for gf logging

--- src/deltix/util/log/gf/impl/SafeAppenderFactory.java
+++ src/deltix/util/log/gf/impl/SafeAppenderFactory.java
@@ -1,39 +1,37 @@
 package deltix.util.log.gf.impl;
 
-
-import org.gflogger.Appender;
-import org.gflogger.LogEntryItem;
 import org.gflogger.LoggerService;
-import org.gflogger.appender.AbstractAppenderFactory;
-import org.gflogger.appender.AppenderFactory;
 
-public class SafeAppenderFactory extends AbstractAppenderFactory {
+public class SafeAppenderFactory extends CompoundAppenderFactory {
 
-    private static final String MAX_ENTRIES_PER_SECOND_PROPERTY_KEY = "gflogger.safeAppender.maxEntriesPerSecond";
-    private static final int DEFAULT_MAX_ENTRIES_PER_SECOND = 500;
+    static final String MAX_ENTRIES_PER_SECOND_PROPERTY_KEY = "gflogger.safeAppender.maxEntriesPerSecond";
+    static final int DEFAULT_MAX_ENTRIES_PER_SECOND = 300;
 
-    private AppenderFactory[] factories;
-
-    public void setFactories(AppenderFactory[] factories) {
-        this.factories = factories;
-    }
+    int maxEntriesPerSecond = getDefaultMaxEntriesPerSecond();
 
     @Override
-    public Appender createAppender(Class<? extends LoggerService> loggerServiceClass) {
-        if (factories != null && factories.length > 0) {
-            Appender[] appenders = new Appender[factories.length];
+    public SafeAppender createAppender(Class<? extends LoggerService> loggerServiceClass) {
 
-            for (int i = 0; i < appenders.length; i++)
-                appenders[i] = factories[i].createAppender(loggerServiceClass);
+        SafeAppender appender = new SafeAppender(createAppenders(loggerServiceClass), maxEntriesPerSecond);
 
-            @SuppressWarnings("unchecked") Appender<LogEntryItem>[] apps = appenders;
-            return new SafeAppender(apps, getMaxEntriesPerSecond());
-        } else {
-            return new NullAppender(multibyte);
-        }
+        appender.setLogLevel(logLevel);
+        appender.setEnabled(enabled);
+        appender.setIndex(index);
+
+        return appender;
     }
 
-    private static int getMaxEntriesPerSecond() {
+    @SuppressWarnings("unused")
+    public int getMaxEntriesPerSecond() {
+        return maxEntriesPerSecond;
+    }
+
+    @SuppressWarnings("unused")
+    public void setMaxEntriesPerSecond(int maxEntriesPerSecond) {
+        this.maxEntriesPerSecond = maxEntriesPerSecond;
+    }
+
+    static int getDefaultMaxEntriesPerSecond() {
         String maxEntriesPerSecondStr = System.getProperty(MAX_ENTRIES_PER_SECOND_PROPERTY_KEY);
         return maxEntriesPerSecondStr == null ?
                 DEFAULT_MAX_ENTRIES_PER_SECOND :
