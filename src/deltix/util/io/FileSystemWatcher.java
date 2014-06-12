896cfd0d9448decc51798d6ce04ddd802723320c
andy <amalakov@deltixlab.com>
2014-06-12 14:52:57 +0000

    Removed debug logging

--- src/deltix/util/io/FileSystemWatcher.java
+++ src/deltix/util/io/FileSystemWatcher.java
@@ -177,7 +177,7 @@ public class FileSystemWatcher {
             setDaemon(true);
             
             try {
-                System.out.println("Preparing WatchingThread");
+                //System.out.println("Preparing WatchingThread");
                 watcher = FileSystems.getDefault().newWatchService();
             } catch (IOException e) {
                 throw new RuntimeException(e);
