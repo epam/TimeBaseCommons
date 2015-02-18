5bb02a58b6a8600751b07169b3c01cfe4bf054f0
rkisel <rkisel@deltixlab.com>
2015-02-18 08:59:30 +0000

    RAF latency test has been added.

--- src/deltix/util/os/NamedPipe.java
+++ src/deltix/util/os/NamedPipe.java
@@ -3,41 +3,43 @@ package deltix.util.os;
 import deltix.util.lang.Disposable;
 import deltix.util.lang.Util;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 
 /**
  *
  */
 public class NamedPipe implements Disposable {
-    RandomAccessFile pipe = null;
-    byte[] readBuf = new byte[2048];
+    final private RandomAccessFile pipe;
+    private byte[] readBuf = new byte[2048];
 
-    public                  NamedPipe() {
-    }
-
-    public void             open(String name, String mode) throws IllegalArgumentException, FileNotFoundException {
+    public NamedPipe(String name, String mode) {
         if (!Util.IS_WINDOWS_OS && mode == "rw")
             throw new IllegalArgumentException("Bidirection mode available only for Windows.");
 
-        pipe = new RandomAccessFile(name, mode);
+        try {
+            pipe = new RandomAccessFile(name, mode);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
-    public void             close() {
+    public synchronized void             close() {
         try {
             pipe.close();
         } catch (Exception e) {
             Util.LOGGER.warning(e.getMessage());
         }
-        pipe = null;
     }
 
-    public void             writeString(String msg) throws IOException {
+    public synchronized RandomAccessFile getRAF() {
+        return pipe;
+    }
+
+    public synchronized void             writeString(String msg) throws IOException {
         pipe.write(msg.getBytes());
     }
 
-    public String           readString() throws IOException {
+    public synchronized String           readString() throws IOException {
         int count = pipe.read(readBuf);
         if (count > 0)
             return new String(readBuf, 0, count, "UTF-8");
