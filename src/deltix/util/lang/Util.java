8090369c08503209c67b414b780001c30cf8aa1d
ndul <ndul@deltixlab.com>
2015-02-20 16:20:27 +0000

    Merged revision(s) 31436-31443 from 30B

--- src/deltix/util/lang/Util.java
+++ src/deltix/util/lang/Util.java
@@ -188,7 +188,56 @@ public class Util {
         }
 
         return (diff);
-        
+    }
+
+    /**
+     * Searches target CharSequence in the source.
+     * The source is the character sequence being searched, and the target
+     * is the character sequence being searched for.
+     *
+     * @param   source       the characters being searched.
+     * @param   sourceOffset offset of the source string.
+     * @param   sourceCount  count of the source string.
+     * @param   target       the characters being searched for.
+     * @param   targetOffset offset of the target string.
+     * @param   targetCount  count of the target string.
+     * @param   fromIndex    the index to begin searching from.
+     */
+    public static int indexOf(CharSequence source, int sourceOffset, int sourceCount,
+                              CharSequence target, int targetOffset, int targetCount,
+                              int fromIndex) {
+        if (fromIndex >= sourceCount) {
+            return (targetCount == 0 ? sourceCount : -1);
+        }
+        if (fromIndex < 0) {
+            fromIndex = 0;
+        }
+        if (targetCount == 0) {
+            return fromIndex;
+        }
+
+        char first = target.charAt(targetOffset);
+        int max = sourceOffset + (sourceCount - targetCount);
+
+        for (int i = sourceOffset + fromIndex; i <= max; i++) {
+            /* Look for first character. */
+            if (source.charAt(i) != first) {
+                while (++i <= max && source.charAt(i) != first);
+            }
+
+            /* Found first character, now look at the rest of v2 */
+            if (i <= max) {
+                int j = i + 1;
+                int end = j + targetCount - 1;
+                for (int k = targetOffset + 1; j < end && source.charAt(j) == target.charAt(k); j++, k++);
+
+                if (j == end) {
+                    /* Found whole string. */
+                    return i - sourceOffset;
+                }
+            }
+        }
+        return -1;
     }
 
     public static <T extends Comparable <T>> T  max (T a, T b) {
