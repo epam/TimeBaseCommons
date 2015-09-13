5d2033a9a165eb291aca711c301ed5a5eb2b8e41
andy <amalakov@deltixlab.com>
2015-09-13 19:00:45 +0000

    Variable entry size cache

---
+++ src/deltix/util/collections/ByteArray.java
@@ -0,0 +1,63 @@
+package deltix.util.collections;
+
+/**
+ * Similar to ByteArrayList but not resizable (although supports flyweight pattern)
+ */
+public class ByteArray {
+
+    private byte[] array;
+    private int offset;
+    private int length;
+
+    public ByteArray() {
+        array = new byte[0];
+    }
+
+    public ByteArray(byte [] array) {
+        this.array = array;
+        offset = 0;
+        length = array.length;
+    }
+
+    public ByteArray(byte [] array, int offset, int length) {
+        this.array = array;
+        this.offset = offset;
+        this.length = length;
+    }
+
+    public ByteArray(int length) {
+        array = new byte[length];
+        offset = 0;
+        this.length = length;
+    }
+
+
+    public void setArray(byte [] array, int offset, int length) {
+        this.array = array;
+        this.offset = offset;
+        this.length = length;
+    }
+
+    public byte get(int index) {
+        int i = index + offset;
+        if (index > length)
+            throw new ArrayIndexOutOfBoundsException(index);
+        return array[index + offset];
+    }
+
+    public byte[] getArray() {
+        return array;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
