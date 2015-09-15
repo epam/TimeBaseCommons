e56a2b5a65c16d2967c31f0fdd2e3180c0d538ca
andy <amalakov@deltixlab.com>
2015-09-15 16:47:02 +0000

    Tuneup default logging

--- src/deltix/util/log/gf/jul/JULLoggerFactory.java
+++ src/deltix/util/log/gf/jul/JULLoggerFactory.java
@@ -1,10 +1,33 @@
 package deltix.util.log.gf.jul;
 
 
+import deltix.util.log.TerseFormatter;
 import deltix.util.log.gf.Logger;
 import deltix.util.log.gf.LoggerFactory;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.LogManager;
+
 public final class JULLoggerFactory extends LoggerFactory {
+
+    public static final String CONFIGURATION_LOADED_FLAG = "deltix.util.log.gf.jul.custom.configuration.loaded";
+
+    public JULLoggerFactory() {
+        LogManager logManager = LogManager.getLogManager();
+        if ( ! Boolean.parseBoolean(logManager.getProperty(CONFIGURATION_LOADED_FLAG))) {
+
+            java.util.logging.Logger root = logManager.getLogger("");
+            Handler [] handlers = root.getHandlers();
+            for(Handler handler : handlers)
+                root.removeHandler(handler);
+
+            ConsoleHandler console = new ConsoleHandler();
+            root.addHandler(console);
+            console.setFormatter(new TerseFormatter());
+
+        }
+    }
 
     @Override
     protected Logger createLogger(String name) {
