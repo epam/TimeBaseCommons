208385753f033399ec44af7ff15851d11a14c6f0
andy <amalakov@deltixlab.com>
2015-09-15 16:52:33 +0000

    Variable entry size cache

--- src/deltix/util/log/gf/jul/JULLoggerFactory.java
+++ src/deltix/util/log/gf/jul/JULLoggerFactory.java
@@ -16,7 +16,7 @@ public final class JULLoggerFactory extends LoggerFactory {
     public JULLoggerFactory() {
         LogManager logManager = LogManager.getLogManager();
         if ( ! Boolean.parseBoolean(logManager.getProperty(CONFIGURATION_LOADED_FLAG))) {
-
+            // Let's use terse log format by default (otherwise we will see JULLogger.commit() as log source for each log entry
             java.util.logging.Logger root = logManager.getLogger("");
             Handler [] handlers = root.getHandlers();
             for(Handler handler : handlers)
