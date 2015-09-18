e025fbd69278cf5719876197be0a1cb68bddd783
akarpovich <akarpovich@deltixlab.com>
2015-09-18 14:42:45 +0000

    [-] subscriptions

--- src/deltix/util/collections/MultiMap.java
+++ src/deltix/util/collections/MultiMap.java
@@ -6,17 +6,19 @@ import java.util.*;
  *  Our version of MultiMap. Thanks to Sun, everyone has their own.
  */
 public interface MultiMap <K, V> {
-    public void                 clear ();
+    public void                         clear ();
 
-    public Collection <V>       remove (K key);
+    public Collection <V>               remove (K key);
 
-    public Collection <V>       put (K key, V value);
+    public Collection <V>               put (K key, V value);
     
-    public Collection <V>       get (K key);
+    public Collection <V>               get (K key);
 
-    public boolean              isEmpty();
+    public boolean                      isEmpty();
 
-    public int                  size();
+    public int                          size();
 
-    public Set<K>               keySet();
+    public Set<K>                       keySet();
+
+    public Collection<Collection<V>>    values();
 }
