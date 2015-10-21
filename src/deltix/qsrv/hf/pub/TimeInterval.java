99ac705b20f3a450e472955dc2facdf75022f87e
akarpovich <akarpovich@deltixlab.com>
2015-10-21 13:28:23 +0000

    [-] getTimeRange() performance optimizations; [-] empty data blocks support;

---
+++ src/deltix/qsrv/hf/pub/TimeInterval.java
@@ -0,0 +1,14 @@
+package deltix.qsrv.hf.pub;
+
+public interface TimeInterval {
+
+    /*
+        Returns start time is measured in nanoseconds that passed since January 1, 1970 UTC
+     */
+    public long         getFromTime();
+
+    /*
+        Returns end time is measured in nanoseconds that passed since January 1, 1970 UTC
+     */
+    public long         getToTime();
+}
