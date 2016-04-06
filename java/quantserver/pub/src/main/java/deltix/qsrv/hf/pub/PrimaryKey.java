509cadb0f7031f5b5c98208f284c13f4bb63cb31
ndul <ndul@deltixlab.com>
2016-04-06 10:32:06 +0000

    [*] dependency cleanup

--- java/quantserver/pub/src/main/java/deltix/qsrv/hf/pub/PrimaryKey.java
+++ java/quantserver/pub/src/main/java/deltix/qsrv/hf/pub/PrimaryKey.java
@@ -5,8 +5,6 @@ import java.lang.annotation.*;
 /**
  * Tags a field as part of the message record's primary key (record identity).
  * This tag can be applied to more than one field of single class (composite primary key).
- *
- * Messages containing primary key can be used in {@link deltix.qsrv.hf.pub.UniqueMessageChannel}.
  */
 @Documented
 @Retention (RetentionPolicy.RUNTIME)
