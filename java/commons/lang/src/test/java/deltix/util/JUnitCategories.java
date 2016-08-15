b3d45e9175e27d930ea195a5700df21319a7b50d
vpastushenya <vpastushenya@deltixlab.com>
2016-08-15 17:43:46 +0000

    JUnit tests on Gradle corrections

--- java/commons/lang/src/test/java/deltix/util/JUnitCategories.java
+++ java/commons/lang/src/test/java/deltix/util/JUnitCategories.java
@@ -1,9 +1,29 @@
 package deltix.util;
 
-//If you want add new category then you should add his name to allTestCategories list in java/build.gradle
+/**
+ * Note: If you choose TickDB category all child categories will be executed
+ * When you want to group your categories you should specify children names like ${parent_name}${some_extension} (e.g. TickDB)
+ */
 public final class JUnitCategories {
     public interface Utils {}
 
+    public interface UHFFramework {}
+
+    public interface TickDB {}
+
+    public interface TickDBFast extends TickDB {}
+
+    public interface TickDBQQL extends TickDB {}
+
+    public interface TickDBSlow extends TickDB {}
+
+    public interface TickDBStress extends TickDB {}
+
+    public interface RAMDisk {}
+
+    public interface UHFUtils {}
+
+    //Internal
     public interface NonParallel {}
 }
 
