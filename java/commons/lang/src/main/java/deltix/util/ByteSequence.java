a1dfeee5bf88ff4c996a614ae1fec4b0f274ecad
akorzun <akorzun@deltixlab.com>
2016-10-03 17:26:01 +0000

    added buffer, byte sequence, utils

---
+++ java/commons/lang/src/main/java/deltix/util/ByteSequence.java
@@ -0,0 +1,46 @@
+package deltix.util;
+
+import deltix.util.buffer.Buffer;
+
+
+public interface ByteSequence extends CharSequence {
+
+    byte byteAt(int index);
+
+    Buffer buffer();
+
+    default char charAt(int index) {
+        return (char) byteAt(index);
+    }
+
+    default boolean equals(CharSequence sequence) {
+        if(sequence == null)
+            return false;
+
+        int length = length();
+        if (sequence.length() != length)
+            return false;
+
+        for (int i = 0; i < length; i++)
+            if (byteAt(i) != sequence.charAt(i))
+                return false;
+
+        return true;
+    }
+
+    default boolean equals(ByteSequence sequence) {
+        if(sequence == null)
+            return false;
+
+        int length = length();
+        if (sequence.length() != length)
+            return false;
+
+        for (int i = 0; i < length; i++)
+            if (byteAt(i) != sequence.byteAt(i))
+                return false;
+
+        return true;
+    }
+
+}
