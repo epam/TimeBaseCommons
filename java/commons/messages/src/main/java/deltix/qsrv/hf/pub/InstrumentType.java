626072a37575d053ff85cc29fc1bcb615e029fe8
avavilau <avavilau@deltixlab.com>
2016-10-22 00:08:43 +0000

    32-A-1.3.2: CONTRACT_FOR_DIFFERENSE

--- java/commons/messages/src/main/java/deltix/qsrv/hf/pub/InstrumentType.java
+++ java/commons/messages/src/main/java/deltix/qsrv/hf/pub/InstrumentType.java
@@ -21,6 +21,9 @@ public enum InstrumentType {
     STREAM('M', "Stream"),
     DATA_CONNECTOR('Q', "DataConnector"),
     EXCHANGE_TRADED_SYNTHETIC('Z', "Exchange-Traded Synthetic"),
+    
+    CONTRACT_FOR_DIFFERENSE('D', "Contrach-For-DifferenseSynthetic"),
+
 
     SYSTEM('X', "System");
 
