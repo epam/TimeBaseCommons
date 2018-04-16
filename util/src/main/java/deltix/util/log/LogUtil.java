5b53bf7af0781afeb392a0430478cf3b9ea54030
Alex Karpovich <akarpovich@deltixlab.com>
2018-04-16 14:42:31 +0300

    [*] default logger

---
+++ util/src/main/java/deltix/util/log/LogUtil.java
@@ -0,0 +1,12 @@
+package deltix.util.log;
+
+import deltix.gflog.Log;
+import deltix.gflog.LogFactory;
+
+/**
+ * Created by Alex Karpovich on 4/16/2018.
+ */
+public class LogUtil {
+    public static final String   LOGGER_NAME        = "deltix.util";
+    public static final Log      LOGGER             = LogFactory.getLog(LOGGER_NAME);
+}
