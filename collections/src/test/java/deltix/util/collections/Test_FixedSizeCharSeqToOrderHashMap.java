3421ee3535872bfa698406e9a76f1fa24c65528b
Andy Malakov <amalakov@deltixlab.com>
2018-09-19 11:02:08 -0400

    added assertion

--- collections/src/test/java/deltix/util/collections/Test_FixedSizeCharSeqToOrderHashMap.java
+++ collections/src/test/java/deltix/util/collections/Test_FixedSizeCharSeqToOrderHashMap.java
@@ -152,6 +152,11 @@ public class Test_FixedSizeCharSeqToOrderHashMap {
         assertContent("84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99");
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testSingletonMap() {
+        FixedSizeCharSeqToObjectMap<Long> singleton = new FixedSizeCharSeqToObjectMap<>(1, null);
+    }
+
 
     private void insert(int key, Long value) {
         insert (Integer.toString(key), value);
