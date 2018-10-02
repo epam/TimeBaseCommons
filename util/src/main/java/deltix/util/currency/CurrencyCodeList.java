3b021bfbe814aa2cdc5e52b58bbb7b34013438b4
Alex Karpovich <akarpovich@deltixlab.com>
2018-10-02 15:02:50 +0300

    [*] currencies: allow > 1000

--- util/src/main/java/deltix/util/currency/CurrencyCodeList.java
+++ util/src/main/java/deltix/util/currency/CurrencyCodeList.java
@@ -23,7 +23,7 @@ import java.util.logging.Level;
 @Depends("deltix/util/currency/CurrencyCodes.xml")
 @ThreadSafe
 public class CurrencyCodeList {
-    private static final int                             amount        =   1000;
+    private static final int                             amount        =   10000;
     private static final CurrencyInfo[]                  numericIndex  = new CurrencyInfo[amount];
     private static final ThreeLetterToObjectMapQuick<CurrencyInfo> symbolicIndex = new ThreeLetterToObjectMapQuick<>(amount);
 
