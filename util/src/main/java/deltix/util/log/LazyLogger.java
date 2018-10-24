9aebacf325fdf333d88b3622ccadce620665524a
Andy Malakov <amalakov@deltixlab.com>
2018-10-24 17:04:20 -0400

    Marked as deprecated

--- util/src/main/java/deltix/util/log/LazyLogger.java
+++ util/src/main/java/deltix/util/log/LazyLogger.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 /**
  * Logger uses lazy initialization on first log() in background thread.
  */
+@Deprecated
 public class LazyLogger {
 
     private final ArrayDeque<LogRecord> buffer = new ArrayDeque<LogRecord>(20);
