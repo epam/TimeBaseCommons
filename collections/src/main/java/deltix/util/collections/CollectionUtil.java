c1d417eee0c00f044f395c306b6e62c94b56d256
Artyom Korzun <akorzun@deltixlab.com>
2018-11-19 13:23:25 +0300

    1. Added a copy constructor to primitive lists which takes the interface of a list 2. Added hashCode, equals, copy methods to CollectionUtil

--- collections/src/main/java/deltix/util/collections/CollectionUtil.java
+++ collections/src/main/java/deltix/util/collections/CollectionUtil.java
@@ -1,74 +1,70 @@
 package deltix.util.collections;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
+import deltix.util.collections.generated.*;
 import deltix.util.lang.Filter;
 import deltix.util.lang.StringUtils;
 import deltix.util.lang.Transformer;
 import deltix.util.lang.Util;
 
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.function.Function;
+
 public class CollectionUtil {
 
-    public static <T> void remove (Collection<T> collection,
-                                   Filter<T> filter) {
-        ArrayList<T> r = new ArrayList<T> ();
+    public static <T> void remove(Collection<T> collection,
+                                  Filter<T> filter) {
+        ArrayList<T> r = new ArrayList<T>();
         for (T o : collection) {
-            if (filter.accept (o))
-                r.add (o);
+            if (filter.accept(o))
+                r.add(o);
         }
 
         for (T o : r) {
-            collection.remove (o);
+            collection.remove(o);
         }
     }
 
-    public static String toString (Collection<?> collection, String separator) {
-        return toString (collection, "", "", separator);
+    public static String toString(Collection<?> collection, String separator) {
+        return toString(collection, "", "", separator);
     }
 
-    public static String toString (Collection<?> collection, String head, String tail, String separator) {
-        if (collection == null || collection.isEmpty ())
+    public static String toString(Collection<?> collection, String head, String tail, String separator) {
+        if (collection == null || collection.isEmpty())
             return head + tail;
 
-        StringBuilder builder = new StringBuilder (128);
-        builder.append (head);
+        StringBuilder builder = new StringBuilder(128);
+        builder.append(head);
         for (Object value : collection)
-            builder.append (value).append (separator);
-        builder.setLength (builder.length () - separator.length ());
-        builder.append (tail);
+            builder.append(value).append(separator);
+        builder.setLength(builder.length() - separator.length());
+        builder.append(tail);
         return builder.toString();
     }
 
-    public static <T extends Enum<T>> EnumSet<T> toEnumSet (Class<T> elementType,
-                                                            String... elements) throws IllegalArgumentException {
+    public static <T extends Enum<T>> EnumSet<T> toEnumSet(Class<T> elementType,
+                                                           String... elements) throws IllegalArgumentException {
         if (elements == null || elements.length <= 0)
-            return EnumSet.noneOf (elementType);
+            return EnumSet.noneOf(elementType);
 
-        EnumSet<T> result = EnumSet.noneOf (elementType);
+        EnumSet<T> result = EnumSet.noneOf(elementType);
         for (String element : elements) {
-            String elementStr = StringUtils.trim (element);
+            String elementStr = StringUtils.trim(element);
             if (elementStr != null)
-                result.add (Enum.valueOf (elementType, elementStr));
+                result.add(Enum.valueOf(elementType, elementStr));
         }
         return result;
     }
 
     @SuppressWarnings("unchecked")
-    public static <T> T[] remove (T[] elementData, T element, Class<T> clazz) {
+    public static <T> T[] remove(T[] elementData, T element, Class<T> clazz) {
         for (int index = 0; index < elementData.length; index++)
-            if (Util.xequals (element, elementData[index])) {
+            if (Util.xequals(element, elementData[index])) {
                 int numMoved = elementData.length - index - 1;
                 if (numMoved >= 0) {
-                    T[] result = (T[]) Array.newInstance (clazz, elementData.length - 1);
-                    System.arraycopy (elementData, 0, result, 0, index);
-                    System.arraycopy (elementData, index + 1, result, index, numMoved);
+                    T[] result = (T[]) Array.newInstance(clazz, elementData.length - 1);
+                    System.arraycopy(elementData, 0, result, 0, index);
+                    System.arraycopy(elementData, index + 1, result, index, numMoved);
                     return result;
                 }
             }
@@ -100,4 +96,313 @@ public class CollectionUtil {
             Array.set(array, count++, value);
         return array;
     }
+
+    // region hashCode
+
+    public static int hashCode(final ObjectList<?> list) {
+        int hash = 0;
+
+        if (list != null) {
+            for (int i = 0, size = list.size(); i < size; i++) {
+                final Object object = list.getObjectNoRangeCheck(i);
+                hash = 31 * hash + Objects.hashCode(object);
+            }
+        }
+
+        return hash;
+    }
+
+    public static int hashCode(final LongList list) {
+        int hash = 0;
+
+        if (list != null) {
+            for (int i = 0, size = list.size(); i < size; i++) {
+                final long value = list.getLongNoRangeCheck(i);
+                hash = 31 * hash + Long.hashCode(value);
+            }
+        }
+
+        return hash;
+    }
+
+    public static int hashCode(final IntegerList list) {
+        int hash = 0;
+
+        if (list != null) {
+            for (int i = 0, size = list.size(); i < size; i++) {
+                final int value = list.getIntegerNoRangeCheck(i);
+                hash = 31 * hash + Integer.hashCode(value);
+            }
+        }
+
+        return hash;
+    }
+
+    public static int hashCode(final ShortList list) {
+        int hash = 0;
+
+        if (list != null) {
+            for (int i = 0, size = list.size(); i < size; i++) {
+                final short value = list.getShortNoRangeCheck(i);
+                hash = 31 * hash + Short.hashCode(value);
+            }
+        }
+
+        return hash;
+    }
+
+    public static int hashCode(final ByteList list) {
+        int hash = 0;
+
+        if (list != null) {
+            for (int i = 0, size = list.size(); i < size; i++) {
+                final byte value = list.getByteNoRangeCheck(i);
+                hash = 31 * hash + Byte.hashCode(value);
+            }
+        }
+
+        return hash;
+    }
+
+    public static int hashCode(final CharacterList list) {
+        int hash = 0;
+
+        if (list != null) {
+            for (int i = 0, size = list.size(); i < size; i++) {
+                final char value = list.getCharacterNoRangeCheck(i);
+                hash = 31 * hash + Character.hashCode(value);
+            }
+        }
+
+        return hash;
+    }
+
+    public static int hashCode(final BooleanList list) {
+        int hash = 0;
+
+        if (list != null) {
+            for (int i = 0, size = list.size(); i < size; i++) {
+                final boolean value = list.getBooleanNoRangeCheck(i);
+                hash = 31 * hash + Boolean.hashCode(value);
+            }
+        }
+
+        return hash;
+    }
+
+    // endregion
+
+    // region equals
+
+    public static <T> boolean equals(final ObjectList<T> lhs, final ObjectList<T> rhs) {
+        if (lhs == rhs) {
+            return true;
+        }
+
+        if (lhs == null || rhs == null) {
+            return false;
+        }
+
+        final int size = lhs.size();
+        if (size != rhs.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (!Objects.equals(lhs.getObjectNoRangeCheck(i), rhs.getObjectNoRangeCheck(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean equals(final LongList lhs, final LongList rhs) {
+        if (lhs == rhs) {
+            return true;
+        }
+
+        if (lhs == null || rhs == null) {
+            return false;
+        }
+
+        final int size = lhs.size();
+        if (size != rhs.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (lhs.getLongNoRangeCheck(i) != rhs.getLongNoRangeCheck(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean equals(final IntegerList lhs, final IntegerList rhs) {
+        if (lhs == rhs) {
+            return true;
+        }
+
+        if (lhs == null || rhs == null) {
+            return false;
+        }
+
+        final int size = lhs.size();
+        if (size != rhs.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (lhs.getIntegerNoRangeCheck(i) != rhs.getIntegerNoRangeCheck(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean equals(final ShortList lhs, final ShortList rhs) {
+        if (lhs == rhs) {
+            return true;
+        }
+
+        if (lhs == null || rhs == null) {
+            return false;
+        }
+
+        final int size = lhs.size();
+        if (size != rhs.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (lhs.getShortNoRangeCheck(i) != rhs.getShortNoRangeCheck(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean equals(final ByteList lhs, final ByteList rhs) {
+        if (lhs == rhs) {
+            return true;
+        }
+
+        if (lhs == null || rhs == null) {
+            return false;
+        }
+
+        final int size = lhs.size();
+        if (size != rhs.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (lhs.getByteNoRangeCheck(i) != rhs.getByteNoRangeCheck(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean equals(final CharacterList lhs, final CharacterList rhs) {
+        if (lhs == rhs) {
+            return true;
+        }
+
+        if (lhs == null || rhs == null) {
+            return false;
+        }
+
+        final int size = lhs.size();
+        if (size != rhs.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (lhs.getCharacterNoRangeCheck(i) != rhs.getCharacterNoRangeCheck(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean equals(final BooleanArrayList lhs, final BooleanArrayList rhs) {
+        if (lhs == rhs) {
+            return true;
+        }
+
+        if (lhs == null || rhs == null) {
+            return false;
+        }
+
+        final int size = lhs.size();
+        if (size != rhs.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (lhs.getBooleanNoRangeCheck(i) != rhs.getBooleanNoRangeCheck(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // endregion
+
+    // region copy
+
+    @SuppressWarnings("unchecked")
+    public static <T> ObjectArrayList<T> copy(final ObjectList<T> list,
+                                              final Function<? super T, ? extends T> elementCopyFunction) {
+        if (list == null) {
+            return null;
+        }
+
+        final int size = list.size();
+        final ObjectArrayList<T> copy = new ObjectArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            final T element = list.getObjectNoRangeCheck(i);
+            final T elementCopy = elementCopyFunction.apply(element);
+
+            copy.add(elementCopy);
+        }
+
+        return copy;
+    }
+
+    public static LongArrayList copy(final LongList list) {
+        return (list == null) ? null : new LongArrayList(list);
+    }
+
+    public static IntegerArrayList copy(final IntegerList list) {
+        return (list == null) ? null : new IntegerArrayList(list);
+    }
+
+    public static ShortArrayList copy(final ShortList list) {
+        return (list == null) ? null : new ShortArrayList(list);
+    }
+
+    public static ByteArrayList copy(final ByteList list) {
+        return (list == null) ? null : new ByteArrayList(list);
+    }
+
+    public static CharacterArrayList copy(final CharacterList list) {
+        return (list == null) ? null : new CharacterArrayList(list);
+    }
+
+    public static BooleanArrayList copy(final BooleanList list) {
+        return (list == null) ? null : new BooleanArrayList(list);
+    }
+
+    // endregion
+
 }
