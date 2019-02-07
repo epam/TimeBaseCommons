d821c28659d7553c4c265edd99d7447d7153313a
Aliaksei Vavilau <avavilau@deltixlab.com>
2019-02-07 04:02:37 +0300

    [SKIP-CI] Add Poolable Object List.

---
+++ collections/src/main/java/deltix/util/collections/PoolableObjectList.java
@@ -0,0 +1,16 @@
+package deltix;
+
+import deltix.util.collections.generated.ObjectList;
+
+public interface PoolableObjectList<T> extends ObjectList<T> {
+
+    int capacity();
+
+    void clear();
+
+    <Q extends T> void hintCapacity(int capacity, Class<Q> type);
+
+    <Q extends T> void setSize(int count, Class<Q> type);
+
+    <Q extends T> Q append(Class<Q> type);
+}
