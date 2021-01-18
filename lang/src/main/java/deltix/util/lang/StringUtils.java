b8077cbc99c7f8c41c5895ea4c6f04848e45e35e
Alex Karpovich <akarpovich@deltixlab.com>
2021-01-18 18:20:14 +0300

    [*] java reserved words

--- lang/src/main/java/deltix/util/lang/StringUtils.java
+++ lang/src/main/java/deltix/util/lang/StringUtils.java
@@ -22,19 +22,21 @@ public class StringUtils {
     public static       String precisionTemplate       = "(\\d{%s}\\z)|(\\d{3},\\d{3}\\z)|(\\d{3},\\d{3},\\d{3}\\z)";
 
     public static String []     JAVA_KEYWORDS = {
-        "assert", "abstract", "boolean", "break", "byte",
-        "case", "catch", "char", "class",
-        "const", "continue", "default", "do",
-        "double", "else", "extends", "false",
-        "final", "finally", "float", "for",
-        "goto", "if", "implements", "import",
-        "instanceof", "int", "interface",
-        "long", "native", "new", "null", "package",
-        "private", "protected", "public",
-        "return", "short", "static", "super",
-        "switch", "synchronized", "this",
-        "throw", "throws", "transient", "true",
-        "try", "void", "volatile", "while"
+            "abstract", "assert",
+            "boolean", "break", "byte",
+            "case", "catch", "char", "class", "const", "continue",
+            "default", "do", "double",
+            "else", "enum", "extends",
+            "false",  "final", "finally", "float", "for",
+            "goto",
+            "if", "implements", "import", "instanceof", "int", "interface",
+            "long", "native", "new", "null",
+            "package", "private", "protected", "public",
+            "return",
+            "short", "static", "strictfp", "super", "switch", "synchronized",
+            "this", "throw", "throws", "transient", "true", "try",
+            "void", "volatile",
+            "while"
     };
 
     public static String []     CS_KEYWORDS = {
