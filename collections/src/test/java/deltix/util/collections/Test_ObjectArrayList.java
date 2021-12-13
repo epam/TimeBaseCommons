6014b9a12121c77d7762e7068cb30f9c630908be
akarpovich <karpovich@deltixlab.com>
2021-12-13 20:14:28 +0300

    [-] https://gitlab.deltixhub.com/Deltix/QuantServer/Common/Commons/-/issues/6

--- collections/src/test/java/deltix/util/collections/Test_ObjectArrayList.java
+++ collections/src/test/java/deltix/util/collections/Test_ObjectArrayList.java
@@ -1,5 +1,6 @@
 package deltix.util.collections;
 
+import deltix.util.collections.generated.IntegerArrayList;
 import deltix.util.collections.generated.ObjectArrayList;
 import org.junit.Assert;
 import org.junit.Test;
@@ -62,6 +63,67 @@ public class Test_ObjectArrayList {
 
         list.clear();
         assertList("[]", list);
+    }
+
+
+    @Test
+    public void testSort() {
+
+        final ObjectArrayList<String> list = new ObjectArrayList<>();
+        list.add("3");
+        list.add("2");
+        list.add("1");
+        list.add("0");
+
+        list.sort();
+
+        assertList("[0, 1, 2, 3]", list);
+
+        list.clear();
+        list.add("3");
+        list.add("2");
+        list.add("1");
+        list.add("0");
+
+        list.sort(0, 2);
+        assertList("[2, 3, 1, 0]", list);
+
+        list.sort(0, 3);
+        assertList("[1, 2, 3, 0]", list);
+
+        list.sort(0, 4);
+        assertList("[0, 1, 2, 3]", list);
+
+    }
+
+    @Test
+    public void testSortPrivitives() {
+
+        final IntegerArrayList list = new IntegerArrayList();
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        list.add(0);
+
+        list.sort();
+
+        assertEquals("{0, 1, 2, 3}", list.toString());
+
+        list.clear();
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        list.add(0);
+
+        list.sort(0, 2);
+        assertEquals("{2, 3, 1, 0}", list.toString());
+
+        list.sort(0, 3);
+        assertEquals("{1, 2, 3, 0}", list.toString());
+
+        list.sort(0, 4);
+        assertEquals("{0, 1, 2, 3}", list.toString());
+
     }
 
     @Test
