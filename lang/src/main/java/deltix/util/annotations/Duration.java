9a3f38fd17d0301898b23ee8019bc40787184290
Alex Karpovich <aliaksandr_karpovich@epam.com>
2023-02-15 16:14:50 +0300

    [*] duration added

---
+++ lang/src/main/java/deltix/util/annotations/Duration.java
@@ -0,0 +1,16 @@
+package deltix.util.annotations;
+
+import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Marks field/parameter/local-variable/method-return-type of integer type which should be considered as duration.
+ */
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface Duration {
+
+    TimeUnit timeUnit() default TimeUnit.MILLISECONDS;
+
+}
