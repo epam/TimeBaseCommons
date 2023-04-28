8c6846a91353ac07624df956b009f5713fd95b1c
Alexei Osipov <Alexei_Osipov@epam.com>
2023-04-28 20:23:52 +0200

    Upgrade to Gradle 8

--- lang/src/main/java/deltix/util/CommonsJUnitCategories.java
+++ lang/src/main/java/deltix/util/CommonsJUnitCategories.java
@@ -1,5 +1,10 @@
 package deltix.util;
 
+/**
+ * @deprecated Use JUnit 5 tags instead ({@link org.junit.jupiter.api.Tag}).
+ * <a href="https://junit.org/junit5/docs/current/user-guide/#writing-tests-tagging-and-filtering">Tagging and Filtering</a>
+ */
+@Deprecated
 public final class CommonsJUnitCategories {
     public interface Unit extends All {}
 
